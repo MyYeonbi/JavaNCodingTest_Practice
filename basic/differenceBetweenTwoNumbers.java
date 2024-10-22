2c015f94f3a42d1ac906d5ec03fd65131d021295
MyYeonbi <yeonbi_kim@icloud.com>
2024-10-22 23:27:34 +0900

    -

---
+++ basic/differenceBetweenTwoNumbers.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class differenceBetweenTwoNumbers {
+    int num1 = 2;
+    int num2 = 3;
+    public int solution(int num1, int num2){
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+}
