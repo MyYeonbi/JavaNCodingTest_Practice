7d30efa59d31442967d88b1d43e7ae7dd8f447bd
MyYeonbi <yeonbi_kim@icloud.com>
2024-11-30 18:35:29 +0900

    :sparkles: Feat : 최대 최소값 구하기2

---
+++ basic/MaxMin2.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class MaxMin2 {
+
+    public String solution(String s) {
+        String answer = "";
+        int min = Integer.MAX_VALUE , max = Integer.MIN_VALUE;
+        int num;
+
+        String[] strArr = s.split(" ");
+
+        for (int i = 0; i < strArr.length; i++) {
+            num = Integer.parseInt(strArr[i]);
+
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+        answer = min + " " + max;
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        MaxMin2 maxMin2 = new MaxMin2();
+        System.out.println((MaxMin2.solution("5 6 7 8 9")));
+    }
+}
