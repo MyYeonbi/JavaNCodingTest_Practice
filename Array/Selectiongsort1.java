2295eb8856b8eac10b408009d7b98e67431fa041
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-04 22:37:50 +0900

    :sparkles: 선택정렬 손코딩하면서 복습

---
+++ Array/Selectiongsort1.java
@@ -0,0 +1,19 @@
+package Array;
+import java.util.Arrays;
+public class Selectiongsort1 {
+    public static void selctionSort(int[] arr) {
+        int n = arr.length;
+
+        for(int i=0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1 ; j < n; j++ ){
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
