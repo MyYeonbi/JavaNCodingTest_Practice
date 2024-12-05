93b07a4d908efa7c3a2143a9840d5653b109cb4c
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-05 20:55:38 +0900

    :sparkles: Feat: String배열의 각요소 길이 세기
    오류 있어서 이슈란에 정리함.

---
+++ Array/Length.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class Length {
+    public int[] solution(String[] strlist) {
+        int n = strlist.length;
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Length length = new Length(); // 객체 생성
+        String[] strlist = {"hello", "world", "java", "coding"};
+        int[] result = length.solution(strlist); // solution 함수 호출
+
+        // 결과 출력
+        for (int len : result) {
+            System.out.print(len + " ");
+        }
+    }
+}
