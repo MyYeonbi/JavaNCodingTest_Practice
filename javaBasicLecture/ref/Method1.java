930a0c909c8dd81bf0d31474169c19f15ef33e65
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-10 08:20:33 +0900

    :sparkles: Feat: 메서드를 만들어서 클래스 필드값 선언을 간단히!- 생성자

--- javaBasicLecture/ref/Method1.java
+++ javaBasicLecture/ref/Method1.java
@@ -3,9 +3,7 @@ package javaBasicLecture.ref;
 public class Method1 {
     public static void main(String[] args) {
         Student student1 = new Student();
-        student1.name = "학생1";
-        student1.age = 15;
-        student1.grade = 90;
+        initStudent(student1, "학생1", 15,90);
 
         Student student2 = new Student();
         student2.name = "학생2";
@@ -21,5 +19,11 @@ public class Method1 {
 
     static void printStudent(Student student) {
         System.out.println("이름: " + student.name + " 나이 :" + student.age + " 성적 : "  + student.grade);
+    }
+
+    static void initStudent(Student student, String name, int age, int grade) {
+        student.name = name;
+        student.age = age;
+        student.grade = grade;
     }
 }
