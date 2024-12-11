d680d3f3e333977bab8fdf61f9dfc11dd96028e1
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-11 10:15:43 +0900

    :sparkles: Feat: 총액 메서드와 출력 메서드를 for-each문으로 표현

--- javaBasicLecture/ref/quiz1/ProductOrderMain2.java
+++ javaBasicLecture/ref/quiz1/ProductOrderMain2.java
@@ -8,11 +8,10 @@ public class ProductOrderMain2 {
         orders[1] = createOrder("김치",5000,2);
         orders[2] = createOrder("콜라",1500,2);
 
-        int totalAmount = 0;
-        for (ProductOrder order : orders) {
-            System.out.println("상품명: " + order.productName + " ,가격:" + order.price + " ,수량: " + order.quantity);
-            totalAmount += order.price + order.quantity;
-        }
+        printOrders(orders);
+        int totalAmount = getTotalAmount(orders);
+        System.out.println("총 결제 금액: " + totalAmount);
+
     }
     static ProductOrder createOrder (String productName, int price, int quantity) {
         ProductOrder order = new ProductOrder();
@@ -20,5 +19,19 @@ public class ProductOrderMain2 {
         order.price = price;
         order.quantity = quantity;
         return order;
+    }
+    static void printOrders(ProductOrder[] orders){
+        for (ProductOrder order : orders) {
+            System.out.println("상품명: " + order.productName + " ,가격:" + order.price + " ,수량: " + order.quantity);
+        }
+
+    }
+
+    static int getTotalAmount(ProductOrder[] orders) {
+        int totalAmount = 0;
+        for (ProductOrder order : orders) {
+            totalAmount += order.price * order.quantity;
+        }
+        return totalAmount;
     }
 }
