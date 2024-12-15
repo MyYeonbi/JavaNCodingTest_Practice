cf0b94dc702558b3fea48172adb70f79f6aacef3
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-15 16:56:04 +0900

    :watermelon: 객체 생성을 여러개 했을때에 내용물은 다르다.

--- recursion/Fibonacci1.java
+++ recursion/Fibonacci1.java
@@ -1,20 +1,21 @@
 package recursion;
 
+//팥붕어빵 틀
 public class Fibonacci1 {
- public static int count = 0;
+ public int count = 0;
 
-    public static int solution(int n) {
+    public int solution(int n) {
         if(n <=1) return n;
-        count++;
+       count++;
         return solution(n-1) + solution(n-2);
     }
 
     public static void main(String[] args) {
         int num = 20;
 
+        
 
-
-        System.out.println(solution(num));
-        System.out.println("총 재귀 몇 번 : " + count);
+        System.out.println(new Fibonacci1().solution(num)); //팥붕어빵 1번 (count =6,solution =3 들어있음)
+        System.out.println("총 재귀 몇 번 : " + new Fibonacci1().count); //팥붕어빵 2번 (count = 0,solution = 0 들어있음 - 팥붕어빵 1번의 count와 solution과는 다른것.)
     }
 }
