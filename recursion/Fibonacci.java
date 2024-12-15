89f6b3322c93e09a3962c2e35a721e552a117fdc
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-15 16:11:10 +0900

    :apple: 재귀는 시간복잡도에서 시간이 많이 걸려서 신중하게 사용할 것!

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -1,15 +1,20 @@
 package recursion;
 
 public class Fibonacci {
+    public  int count = 0;
     public int solution(int n) {
         if(n <=1) return n;
 
+        count++;
         return solution(n-1) + solution(n-2);
     }
 
     public static void main(String[] args) {
-        int num = 4;
+        int num = 20;
 
-        System.out.println(new Fibonacci().solution(num));
+        Fibonacci fibo = new Fibonacci();
+
+        System.out.println(fibo.solution(num));
+        System.out.println("총 재귀 몇 번 : " + fibo.count);
     }
 }
