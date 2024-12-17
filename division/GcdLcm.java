e19677e529b32ee1a2af36de33f5932f80fb7687
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-17 19:31:41 +0900

    :pizza: 출력 메서드가 들어있는 클래스를 public 클래스로 유지시키기 위해 파일을 분리.

--- division/GcdLcm.java
+++ division/GcdLcm.java
@@ -13,19 +13,3 @@ public class GcdLcm {
         return gcd(b, a % b);
     }
 }
- class Main {
-    public static void main(String[] args) {
-        GcdLcm sol = new GcdLcm();
-
-        // 테스트 케이스 실행
-        int[] testCases = {6, 10, 4, 12, 8}; // 테스트할 n 값들
-
-        System.out.println("테스트 케이스 결과:");
-        System.out.println("-----------------------------");
-        for (int n : testCases) {
-            int result = sol.solution(n); //solurion 함수 실행
-            System.out.println("입력: " + n + " -> 필요한 피자 판 수: " + result);
-        }
-
-    }
-}
