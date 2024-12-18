9c09329658d7c80523e80127b7ab656bdd31083f
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-18 16:55:07 +0900

    :pizza: IllegalargumentException을 추가하여 slice수가 2~10제한을 두기

--- division/DividingPizza2.java
+++ division/DividingPizza2.java
@@ -2,11 +2,14 @@ package division;
 
 public class DividingPizza2 {
     public int solution(int slice, int n) {
+        if (slice < 2 || slice > 10) {
+            throw new IllegalArgumentException("slice는 2 이상 10 이하의 값이어야 한다.");
+        }
         return (n + slice -1)/ slice;
     }
     public static void main(String[] args) {
         DividingPizza2 dp = new DividingPizza2();
-        System.out.println(dp.solution(7,10));
+        System.out.println(dp.solution(20,10));
         System.out.println(dp.solution(4,12));
     }
 }
