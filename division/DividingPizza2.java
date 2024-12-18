f6de4389c5be0e7d52f936d8ef9b68220c3ae908
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-18 16:57:18 +0900

    :pizza: try-catch문으로 예외의 상황의 테스트 코드 넣기

--- division/DividingPizza2.java
+++ division/DividingPizza2.java
@@ -9,7 +9,13 @@ public class DividingPizza2 {
     }
     public static void main(String[] args) {
         DividingPizza2 dp = new DividingPizza2();
-        System.out.println(dp.solution(20,10));
+        System.out.println(dp.solution(7,10));
         System.out.println(dp.solution(4,12));
+
+        try {
+            System.out.println(dp.solution(1, 10)); // 예외 발생
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // 출력: slice는 2 이상 10 이하의 값이어야 합니다.
+        }
     }
 }
