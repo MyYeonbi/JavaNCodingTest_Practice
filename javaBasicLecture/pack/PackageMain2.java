cdb3291bf6159787eb4ee5ca5e5edaee18e0bc9b
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-20 00:12:42 +0900

    :cat: import로 패키지 끌어올 때 한꺼번에!

--- javaBasicLecture/pack/PackageMain2.java
+++ javaBasicLecture/pack/PackageMain2.java
@@ -1,10 +1,14 @@
 package javaBasicLecture.pack;
 
-import  javaBasicLecture.pack.a.User;
+import javaBasicLecture.pack.a.*;
+//import  javaBasicLecture.pack.a.User;
+//import javaBasicLecture.pack.a.User2;
+
 public class PackageMain2 {
 
     public static void main(String[] args) {
         Data data = new Data();
-        User user = new  javaBasicLecture.pack.a.User();
+        User user = new User();
+        User2 user2 = new User2();
     }
 }
---
+++ javaBasicLecture/pack/a/User2.java
@@ -0,0 +1,8 @@
+package javaBasicLecture.pack.a;
+
+public class User2 {
+
+    public User2(){
+        System.out.println("패키지 회원 생성.");
+    }
+}
