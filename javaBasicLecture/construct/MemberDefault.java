2c02d9a03266429a799f140bc7121607b574b355
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-19 18:44:55 +0900

    :cat: 기본생성자 직접 설정

---
+++ javaBasicLecture/construct/MemberDefault.java
@@ -0,0 +1,10 @@
+package javaBasicLecture.construct;
+
+public class MemberDefault {
+    String name;
+
+    public MemberDefault() {
+        System.out.println("생성자 호출");
+
+    }
+}
---
+++ javaBasicLecture/construct/MemberDefaultMain.java
@@ -0,0 +1,8 @@
+package javaBasicLecture.construct;
+
+public class MemberDefaultMain {
+
+    public static void main(String[] args) {
+        MemberDefault memberDefault = new MemberDefault();
+    }
+}
