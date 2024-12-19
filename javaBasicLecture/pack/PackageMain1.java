3624af8f02bf2983d3221adf1266b972017b3f2e
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-19 20:30:22 +0900

    :inbox_tray: 자식 클래스에서 객체 생성시 다른 패키지에 있는 부모 클래스를 사용할 때는 ' 폴더이름. '을 붙여줘야 한다.

---
+++ javaBasicLecture/pack/PackageMain1.java
@@ -0,0 +1,9 @@
+package javaBasicLecture.pack;
+
+public class PackageMain1 {
+
+    public static void main(String[] args) {
+        Data data = new Data();
+        javaBasicLecture.pack.a.User user = new  javaBasicLecture.pack.a.User();
+    }
+}
---
+++ javaBasicLecture/pack/a/User.java
@@ -0,0 +1,8 @@
+package javaBasicLecture.pack.a;
+
+public class User {
+
+    public User(){
+        System.out.println("패키지 회원 생성.");
+    }
+}
