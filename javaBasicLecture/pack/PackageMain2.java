861f9689e5559f9fff34bfc684015f6d0db3f833
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-20 00:10:11 +0900

    :cat: 다른 패키지에 있는 부모 클래스를  객체 생성할때 import문을 통해 출처를 객체 생성 할때마다 일일히 기입하지 않아도 된다.

---
+++ javaBasicLecture/pack/PackageMain2.java
@@ -0,0 +1,10 @@
+package javaBasicLecture.pack;
+
+import  javaBasicLecture.pack.a.User;
+public class PackageMain2 {
+
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user = new  javaBasicLecture.pack.a.User();
+    }
+}
