049b1d0eda331f9c1015f18e7d5caa4167ec1c96
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-19 19:17:05 +0900

    :cat: 두개 있는 생성자  중 디폴트 값을 넣은 생성자에 대한 간략한 코드 변경

--- javaBasicLecture/construct/MemberConstruct.java
+++ javaBasicLecture/construct/MemberConstruct.java
@@ -7,9 +7,8 @@ public class MemberConstruct {
 
     //추가
     MemberConstruct(String name, int age) {
-        this.name = name;
-        this.age = age;
-        this.grade = 50;
+        this(name, age, 50); //변경
+
     }
 
     MemberConstruct(String name, int age, int grade) {
