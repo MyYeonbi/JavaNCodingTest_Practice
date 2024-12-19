4599fc93f57f22805405ca45576705fdf14548a9
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-20 00:18:07 +0900

    :cat: 다른 패키지 두개의 동명의 클래스를 가져올 때는 두개  중 하나는 import문으로 끌어올수 없고 객체 생성시 직접 출처를 기입하며 생성해야 한다.

--- javaBasicLecture/pack/PackageMain2.java
+++ javaBasicLecture/pack/PackageMain2.java
@@ -9,6 +9,6 @@ public class PackageMain2 {
     public static void main(String[] args) {
         Data data = new Data();
         User user = new User();
-        User2 user2 = new User2();
+
     }
 }
---
+++ javaBasicLecture/pack/PackageMain3.java
@@ -0,0 +1,13 @@
+package javaBasicLecture.pack;
+
+
+import javaBasicLecture.pack.a.User;
+
+public class PackageMain3 {
+
+    public static void main(String[] args) {
+       User userA = new User();
+       javaBasicLecture.pack.b.User userB = new javaBasicLecture.pack.b.User();
+
+    }
+}
