7eab2d9716a984cd0ad6336a38e03150d87a9dee
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-20 14:55:05 +0900

    :repeat: 생성자 기본값 설정/ 별도의 초기화 코드 없이도 기본적인 상태 유지 가능

---
+++ javaBasicLecture/review/Car.java
@@ -0,0 +1,10 @@
+package javaBasicLecture.review;
+
+public class Car {
+    String color = "Black";
+    int wheels = 4;
+
+    public Car() {
+        // 기본 생성자
+    }
+}
---
+++ javaBasicLecture/review/CarMain.java
@@ -0,0 +1,8 @@
+package javaBasicLecture.review;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println("색깔: "+ car.color + " 바퀴: "+ car.wheels);
+    }
+}
