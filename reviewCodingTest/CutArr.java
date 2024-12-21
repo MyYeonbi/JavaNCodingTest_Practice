238beaa402c2f8c58740538e6018e2e56fc62625
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-21 19:19:30 +0900

    :x: 출력값에 배열의 메모리 주소(hash code)로 출력되는 오류.
    이슈란 참고.

--- reviewCodingTest/CutArr.java
+++ reviewCodingTest/CutArr.java
@@ -1,7 +1,7 @@
 package reviewCodingTest;
 
 public class CutArr {
-    public int[] solution(int[] numbers, int num1, int num2) {
+    public static int[] solution(int[] numbers, int num1, int num2) {
         int [] answer = new int[num2-num1+1];
         for(int i = num1; i<= num2; i++){
             answer[i-num1] = numbers[i];
@@ -11,9 +11,9 @@ public class CutArr {
     }
     public static void main(String[] args) {
         int[] numbers = {1,2,3,4,5,6,7,8,9};
-        int num1 = numbers[3];
-        int num2 = numbers[6];
-        System.out.println(solution(num1,num2));
+        int num1 = 2;
+        int num2 = 6;
+        System.out.println(solution(numbers,num1,num2).toString());
     }
 }
 
