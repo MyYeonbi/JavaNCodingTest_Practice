08a3cbdee1c8aba789a4f27cf9183fbe2f4b63c3
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-21 13:40:23 +0900

    :bee: char[] 배열 문법

---
+++ reviewCodingTest/charA.java
@@ -0,0 +1,11 @@
+package reviewCodingTest;
+
+public class charA {
+
+    public static void main(String[] args) {
+        String str = "yeonbi";
+        char[] charArray = str.toCharArray();
+
+        System.out.println(charArray[1]);
+    }
+}
