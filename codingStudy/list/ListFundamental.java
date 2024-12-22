969ca5f570300544ffff855c28b6a3de54824ccf
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-22 22:36:23 +0900

    :book: ArrayList의 요소 자체를 삭제하는 법과 인덱스로 삭제하는 법

--- codingStudy/list/ListFundamental.java
+++ codingStudy/list/ListFundamental.java
@@ -10,16 +10,17 @@ public class ListFundamental {
 
         //Integer
         List<Integer> numbers = new ArrayList<>();
+
+        for(int i = 1; i< 10; i++){
+            numbers.add(i);
+        }
          //ArrayList, LinkedList
         //0. 배열을 리스트로 전환
         //1. 배열에 숫자를 추가하는 경우
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        System.out.println(numbers);
-        int[] nums = {1,2,3};
-        System.out.println(Arrays.toString(nums));
         //2. 배열에서 숫자 삭제하는 경우
+        numbers.remove(3);
+        numbers.remove(Integer.valueOf(10));
+        System.out.println(numbers);
         //3.
     }
 }
