54c0c3cdb47670a8e5a2cd382e8a7619e7ac4353
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-22 19:08:15 +0900

    :snowman: public 접근제어자는 외부 호출 가능

---
+++ javaBasicLecture/access/a/AccessInnerMain.java
@@ -0,0 +1,10 @@
+package javaBasicLecture.access.a;
+
+public class AccessInnerMain {
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+        //public 호출 가능
+        data.publicField = 1;
+        data.publicMethod();
+    }
+}
