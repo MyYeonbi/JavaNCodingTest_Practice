de925ba41851226073a3b6ecaae248eb4dbe5e6d
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-22 21:46:49 +0900

    :book: 기존 배열에서 마지막 요소 빼기.

--- codingStudy/array/ArrayLimits.java
+++ codingStudy/array/ArrayLimits.java
@@ -9,13 +9,17 @@ public class ArrayLimits {
         System.out.println(Arrays.toString(numbers));
 
         //1. 배열에 숫자 추가하는 경우
-        int numbers2 = new int[numbers.length+1]; //11칸 짜리 만들기
+        int[] numbers2 = new int[numbers.length+1]; //11칸 짜리 만들기
         for (int i = 0; i < numbers.length; i++) {
             numbers2[i] = numbers[i];
         }
         numbers2[numbers.length]=11;
-        //2. 배열에서 숫자 삭제하는 경우
-
+        //2. 배열에서 숫자 삭제하는 경우, 11을 삭제해서 기본 배열 요소 만들기
+        int[] numbers3 = new int[numbers2.length-1];
+        for (int i = 0; i < numbers2.length-1; i++) {
+            numbers3[i] = numbers2[i];
+        }
+        System.out.println(Arrays.toString(numbers3));
         //3. 배열에서 중간값(5) 삭제하는 경우
     }
 }
