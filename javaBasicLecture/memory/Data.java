adb1372035ad858163fc6c5bae522a42709924c1
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-24 17:05:41 +0900

    :santa: static 메모리 구조의 후입선출을 보여주는 출력

---
+++ javaBasicLecture/memory/Data.java
@@ -0,0 +1,13 @@
+package javaBasicLecture.memory;
+
+public class Data {
+    private int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
