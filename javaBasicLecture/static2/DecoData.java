f7b99e32cd807c02c2058e26ecc7f51c3e021cdd
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-24 19:07:14 +0900

    :santa: static 메서드는 내부에 static이 붙은 멤버변수나 메서드만 불러올 수 있다. 인스턴스를 생성해야 하는 나머지 것들은 절대 내부로 불러들일수 없다.

---
+++ javaBasicLecture/static2/DecoData.java
@@ -0,0 +1,24 @@
+package javaBasicLecture.static2;
+
+public class DecoData {
+
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+        // instaceValue++; 인스턴스 변수 접근은 compile error을 일으킨다.
+        // instanceMethod(); 인스턴스 메서드 접근은 compile error을 일으킨다.
+        staticValue++; //정적 변수 접근
+        staticMethod(); //정적 메서드 접근
+
+    }
+
+    private void instanceMethod() {
+        System.out.println("instanceValue"+ instanceValue);
+    }
+    private static void staticMethod() {
+        System.out.println("staticValue"+ staticValue);
+    }
+
+
+}
