abfffb3149ff4744efed83e3cf1ee9ca4b6496d8
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-24 12:32:34 +0900

    :o: totalPrice 기능 추가

--- javaBasicLecture/access/ex/ShoppingCart.java
+++ javaBasicLecture/access/ex/ShoppingCart.java
@@ -31,5 +31,15 @@ public class ShoppingCart {
             Item item = items[i];
             System.out.println("상품명:" + item.getName() + " , 합계 : " + item.getTotalPrice());
         }
+        System.out.println("전체 가격 합:" + calculateTotalPrice());
+    }
+
+    private int calculateTotalPrice() {
+        int totalPrice = 0;
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
+            totalPrice += item.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
