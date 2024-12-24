114eac4444b639cdd606462020639ccc24f4794b
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-24 12:13:01 +0900

    :o: 정답 코드!

--- javaBasicLecture/access/ex/ShoppingCart.java
+++ javaBasicLecture/access/ex/ShoppingCart.java
@@ -4,24 +4,32 @@ public class ShoppingCart {
     // 요구사항 : 1.접근제어자를 사용해서 데이터를 캡슐화 하라.
     // 2. 해당 클래스는 다른 패키지에서도 사용할 수 있어야 한다.
     // 3. 장바구니에는 상품을 최대 10개만 담을수 있다.(10개 초과 등록시 '장바구니가 가득 찼습니다.'출력)
-   private String name;
-   private int price;
-   private int quantity;
-   private int maxQuantity = 10;
 
-    public ShoppingCart(String name, int price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
+  private Item[] items = new Item[10];
+  private int itemCount;
 
-    public void addItem(name, price,quantity) {
-        if (quantity > maxQuantity) {
-            System.out.println("장바구니가 가득 찼습니다.");
-            return;
+  public void addItem(Item item) {
+      if (itemCount >= items.length) {
+          System.out.println("장바구니가 가득 찼습니다.");
+          return;
+      } else {
+          items[itemCount] = item;
+          itemCount++;
+      }
+  }
+
+
+  /*
+       장바구니 상품 출력
+       상품명:마늘, 합계:4000
+       상품명:상추, 합계:12000
+       전체 가격 합: 16000
+   */
+    public void displayItems() {
+        System.out.println("장바구니 상품 출력");
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
+            System.out.println("상품명:" + item.getName() + " , 합계 : " + item.getTotalPrice());
         }
-    }
-    public void displayItem (name, price, quantity){
-        System.out.println(this.name + " " + this.price + " " + this.quantity);
     }
 }
--- javaBasicLecture/access/ex/ShoppingCartMain.java
+++ javaBasicLecture/access/ex/ShoppingCartMain.java
@@ -5,7 +5,7 @@ public class ShoppingCartMain {
         ShoppingCart cart = new ShoppingCart();
 
         Item item1 = new Item("마늘", 2000, 2);
-        Itme item2 = new Item("상추", 3000, 4);
+        Item item2 = new Item("상추", 3000, 4);
 
         cart.addItem(item1);
         cart.addItem(item2);
