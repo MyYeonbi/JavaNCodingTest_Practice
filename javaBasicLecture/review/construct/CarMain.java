34a3c911be3d514c42f08643cf50f2398d3802fc
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-26 15:58:32 +0900

    :bear:

--- javaBasicLecture/review/construct/CarMain.java
+++
@@ -1,8 +0,0 @@
-package javaBasicLecture.review.construct;
-
-public class CarMain {
-    public static void main(String[] args) {
-        Car car = new Car();
-        System.out.println("색깔: "+ car.color + " 바퀴: "+ car.wheels);
-    }
-}
