250302db8043ff24678c9d1e8b6c72a7aacf05f0
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-27 17:08:17 +0900

    :relaxed: 하지만, final Data 속 필드인 value 값은 변경 가능하다.

--- javaBasicLecture/final1/FinalRefMain.java
+++ javaBasicLecture/final1/FinalRefMain.java
@@ -4,6 +4,9 @@ public class FinalRefMain {
     public static void main(String[] args) {
         final Data data ; // 참조형
         data = new Data();
-        data = new Data();
+
+        //참조 대상의 값은 변경 가능.
+        data.value = 10;
+        System.out.println(data.value);
     }
 }
