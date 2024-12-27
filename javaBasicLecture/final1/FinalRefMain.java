c9dbf1931e4d426035b8c1704b5393ec323b783a
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-27 17:05:47 +0900

    :relaxed:  final Data data ;로 설정하고 data = new Data();를 두번 이상 만들 수 없다. 객체 생성도 단 한번밖에 안됀다.

---
+++ javaBasicLecture/final1/FinalRefMain.java
@@ -0,0 +1,9 @@
+package javaBasicLecture.final1;
+
+public class FinalRefMain {
+    public static void main(String[] args) {
+        final Data data ; // 참조형
+        data = new Data();
+        data = new Data();
+    }
+}
