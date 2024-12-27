edd61cfb21f2164285ddae5dcad5d5a84312e2ce
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-27 13:42:03 +0900

    :relaxed: final 변수에 값을 한번 설정해두면 생성자로도 값을 다시 덮을수 없다.

--- javaBasicLecture/final1/FinalInit.java
+++ javaBasicLecture/final1/FinalInit.java
@@ -3,5 +3,10 @@ package javaBasicLecture.final1;
 public class FinalInit {
 
     static final int CONST_VALUE = 10;
-    
+    final int value = 10;
+
+    public FinalInit(int value) {
+        this.value = value;
+    }
+
 }
