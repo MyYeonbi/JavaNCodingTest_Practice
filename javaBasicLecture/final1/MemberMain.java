c5026c34cb496d35782d2742265aead9eb8cc6ea
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-27 17:24:08 +0900

    :relaxed:  private final String id; 는 생성자를 단 한번만 만들 수 있다.

---
+++ javaBasicLecture/final1/MemberMain.java
@@ -0,0 +1,11 @@
+package javaBasicLecture.final1;
+
+public class MemberMain {
+
+    public static void main(String[] args) {
+        Member member = new Member("myid", "kim");
+        member.print();
+        member.changeData("myId2", "seo");
+        member.print();
+    }
+}
