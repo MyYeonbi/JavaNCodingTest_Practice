1d717da4c4574c02ef7ad34b4e9a6524fea7be6c
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-28 22:56:59 +0900

    ❄️ StringBuilder는 Iterable 인터페이스가 아니기 때문에 for-each 문을 사용할수 없고, 정규 for문을 사용해야 한다. (한 글자씩 꺼내는 것은 '.chatAt()' 사용하기.)

---
+++ array/stringArray/stringBuilder/Traversal.java
@@ -0,0 +1,15 @@
+package array.stringArray.stringBuilder;
+
+public class Traversal {
+    public static void main(String[] args) {
+
+
+    StringBuilder sb = new StringBuilder("hello");
+
+    for (int i = 0; i <sb.length();i++){
+        char c = sb.charAt(i);
+        System.out.print(c);
+    }
+    
+    }
+}
