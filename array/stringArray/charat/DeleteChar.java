49d4eaba16fd7417b1b7f4fabc66560eaaad0ce9
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-29 00:57:45 +0900

    ❄️ String에서 지정하는 한 글자를 삭제하는 법(StringBuiler과 charAt()을 사용)

---
+++ array/stringArray/charat/DeleteChar.java
@@ -0,0 +1,21 @@
+package array.stringArray.charat;
+
+public class DeleteChar {
+
+    public String solution(String my_string, String letter) {
+        StringBuilder sb = new StringBuilder(my_string); //StringBuilder생성
+        String str = letter.charAt(0); //letter의 첫 번째 문자를 char로 추출
+
+        for(int i =0; i< sb.length(); i++){ //StringBuilder 순회
+            if(sb.charAt(i) == str){ //문자 비교
+                sb.deleteCharAt(i); // 문자 제거
+                i--; // 제거 후 인덱스 조정
+
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+
+    }
+}
