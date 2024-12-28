a6995110da5281559d7a133e2025725edfa9e795
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-28 15:20:09 +0900

    🍰 자식의 자식 클래스 객체 생성을 하니, C생성자가 출력 되는데 그 안에 B의 생성자, B생성자 안의 A생성자까지 연쇄적으로 출력됨.

--- javaBasicLecture/extends1/access/super2/ClassC.java
+++ javaBasicLecture/extends1/access/super2/ClassC.java
@@ -5,7 +5,7 @@ public class ClassC extends ClassB {
     public ClassC() {
         super(10,20);
         /* ClassB는 생성자를 개발자가 만들었기 때문에 기본 생성자가 자동으로 만들어지지 않는다.
-        그래서, super();를 생략해서는 안된다.  
+        그래서, super();를 생략해서는 안된다.
          */
         System.out.println("ClassC 생성자 ");
     }
