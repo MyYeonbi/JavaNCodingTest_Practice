3e174391c4c4ab9e90ac54074fe328d866a2c023
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-29 22:36:14 +0900

    🍎 ❗ Integer.parseInt()의 매개변수는 문자열(String) 형태만 넣을 수 있습니다. 문자 배열(char[])이나 문자열 배열(String[])은 직접적으로 사용할 수 없다. 오직 String 형태만 매개변수로 넣을수 있다.

--- codingSelfStudy/integger/parse.java
+++ codingSelfStudy/integger/parse.java
@@ -8,7 +8,7 @@ public class parse {
         System.out.println(num);
 
         // 2. 진법을 지정한 변환
-        String binary = "1010" // 2진수 문자열
+        String binary = "1010"; // 2진수 문자열
         int decimal = Integer.parseInt(binary, 2); //  2진수 → 10진수. 출력 : 10
         /* 두번째 매개변수는 첫번째 매개변수로 전달된 문자열이 나타내는 숫자체계(진법)을 의미한다.
         여기서는 2진법의 수를 10진법으로 바꾸려고 하는것이다.
@@ -18,6 +18,18 @@ public class parse {
 	•	10: 10진법 (decimal)
 	•	16: 16진법 (hexadecimal)
          */
+
+        //코테에서 활용법
+
+        //1. 입력 처리 - 여러 숫자가 공백으로 구분된 문자열로 주어지는 경우가 많다. 이를 숫자로 변환할 때 사용한다.
+        String input = "10 20 30";
+        String[] number = input.split(" "); // 공백으로 분리: 분리의 문법은 .split
+        int sum = 0;
+        for (String s : number) {
+            sum += Integer.parseInt(number); //문자열 -> 정수 변형 가능!
+        }
+        System.out.println(sum);
+
 
     }
 }
