436c2ea6620e53d039ba2f37e6115e25832f757b
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-29 21:53:43 +0900

    🍊 String.valueOf()는 문자, 문자 배열,숫자, 불리언, 객체, null를 문자열로 변환할 수 있다.

--- valueOf/StringValue.java
+++ valueOf/StringValue.java
@@ -9,9 +9,30 @@ public class StringValue {
     char[] chars = {'h', 'e', 'l', 'l', 'o'};
     String ch = String.valueOf(chars);
 
+    // 숫자를 문자열로 변환
+    int num = 42;
+    String s = String.valueOf(num);
+
+    // 불리언 값을 문자여로 변환
+    boolean flag = true;
+    String b = String.valueOf(flag);
+
+    // 객체를 문자열로 변환
+    Object obj = new Object();
+    String d = String.valueOf(obj);
+
+    // null값을 문자열로 변환
+    Object o = null;
+    String f = String.valueOf(o);
+
+
     public static void main(String[] args) {
         StringValue sv = new StringValue();
         System.out.println(sv.str);
-        System.out.println(sv.chars);
+        System.out.println(sv.ch);
+        System.out.println(sv.s);
+        System.out.println(sv.b);
+        System.out.println(sv.d);
+        System.out.println(sv.o);
     }
 }
