a50ca43a03d24c8960fca75eb2e5845762f1413f
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-30 00:28:25 +0900

    👶 문자열 회전 문제 처리 - 입력한 문자열을 처리하고 특정 조건에 따라 변형할 때 사용

--- codingSelfStudy/String/splitt/Spllit.java
+++ codingSelfStudy/String/splitt/Spllit.java
@@ -8,5 +8,13 @@ public class Spllit {
         for (String fruit : fruits) {
             System.out.println(fruit);
         }
+
+        // 코테에서의 활용
+        //1. 문자열 회전 문제 처리 - 입력한 문자열을 처리하고 특정 조건에 따라 변형할 때 사용.
+        // 문자열 회전 : "abcde" -> "cdeab"
+        String str1 = "abcde";
+        int rotateBy = 2;
+        String rotated = str1.substring(rotateBy)+str1.substring(0,rotateBy);
+        System.out.println(rotated);  // 출력: cdeab
     }
 }
