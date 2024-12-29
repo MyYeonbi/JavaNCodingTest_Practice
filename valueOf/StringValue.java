ed100d504cdab0cdeb13cf910bd9e965cb9fc69a
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-29 21:47:41 +0900

    🍊 	String.valueOf()는 전달된 인수를 String으로 변환한다, 방금 코드는 문자 배열을 문자열로 바꾸는 코드이다.

--- valueOf/StringValue.java
+++ valueOf/StringValue.java
@@ -1,11 +1,17 @@
 package valueOf;
 
 public class StringValue {
+    // 문자(char)를 문자열로 변환
     char c = 'a';
     String str = String.valueOf(c);
 
+    // 문자 배열(char[])을 문자열로 변환
+    char[] chars = {'h', 'e', 'l', 'l', 'o'};
+    String ch = String.valueOf(chars);
+
     public static void main(String[] args) {
         StringValue sv = new StringValue();
-        System.out.println(sv.str.toString());
+        System.out.println(sv.str);
+        System.out.println(sv.chars);
     }
 }
