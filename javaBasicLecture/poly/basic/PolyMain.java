17f5d71ccbb4437c31b5aa725714b609b61ed997
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-30 18:05:20 +0900

    🐥 Parent 타입의 poly는 Child의 자식 클래스인 GrandChild가 있다면 이것도 참조로서 담을 수 있다. _ Parent poly2 = new GrandChild();

--- javaBasicLecture/poly/basic/PolyMain.java
+++ javaBasicLecture/poly/basic/PolyMain.java
@@ -18,7 +18,10 @@ public class PolyMain {
         // 부모 변수가 자식 인스턴스를 참조(다형적 참조)
         System.out.println("Parent -> Child");
         Parent poly = new Child(); // poly는 Parent타입, 참조는 Child타입.
-        //poly는 Parent 타입이기 때문에 Parent메모리와 Child메모리 중에서 Parent메모리부터 메서드를 찾는다. 
+        //poly는 Parent 타입이기 때문에 Parent메모리와 Child메모리 중에서 Parent메모리부터 메서드를 찾는다.
+        // 또한, Parent 타입의 poly는 Child의 자식 클래스인 GrandChild가 있다면 이것도 참조로서 담을 수 있다.
+        // Parent poly2 = new GrandChild();
+        
         poly.parentMethod();
 
         // Child child1 = new Parent(); // 자식은 부모를 담을 수 없다.
