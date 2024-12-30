ac128a2442020bdff3cc8cd3278a01073bd88c7e
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-30 19:09:32 +0900

    🐥 업캐스팅은 생략해도 되고, 다운캐스팅은 왜 개발자가 직접 명시적으로 캐스팅을 해야할까❓

--- javaBasicLecture/poly/basic/CastingMain3.java
+++ javaBasicLecture/poly/basic/CastingMain3.java
@@ -2,9 +2,14 @@ package javaBasicLecture.poly.basic;
 
 // upcasting vs downcasting
 public class CastingMain3 {
+    public static void main(String[] args) {
 
-    Child child = new Child();
-    Parent parent1 = new Child();
 
-    Parent parent2 = (Parent) Child(); // 업캐스팅은 생략 가능. 생략 권장
+        Child child = new Child();
+        Parent parent1 = new Child();
+        Parent parent2 = child; // 업캐스팅은 생략 가능.// 생략 권장
+
+        parent1.parentMethod();
+        parent2.parentMethod();
+    }
 }
