a0194a6d40d816d557ac5bdf32fe9fab06f86050
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-30 15:20:12 +0900

    ❌ 1.	Book 클래스의 print 메서드 문제,  2.getPrice 메서드 문제

---
+++ javaBasicLecture/extends1/ex/Album.java
@@ -0,0 +1,12 @@
+package javaBasicLecture.extends1.ex;
+
+public class Album extends Item {
+    public String artist;
+
+    public Album(String name, int price, String artist) {
+        super(name, price);
+        this.artist = artist;
+    }
+
+
+}
