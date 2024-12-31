22c43abbf78bde588f3f8e592b9df39eeded4c9d
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-31 16:29:56 +0900

    🍬 부모 변수 + 자식 인스턴스의 객체는 동명의 필드는 부모클래스를 리턴하고 메서드는 '오버라이딩 된 메서드는 항상 우선권을 가진다'이기 때문에 오버라이딩 된 자식클래스의 동명 메서드를 가져온다.

---
+++ javaBasicLecture/poly/overriding/Parent.java
@@ -0,0 +1,10 @@
+package javaBasicLecture.poly.overriding;
+
+public class Parent {
+
+    public String value = "parent";
+
+    public void method() {
+        System.out.println("Parent.method");
+    }
+}
