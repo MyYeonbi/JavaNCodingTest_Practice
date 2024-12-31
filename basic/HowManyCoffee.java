5984a305f408ac0e08f6e368435af2ea0e21eb32
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-31 23:32:07 +0900

    🌳 내가 보유한 돈으로 몇 잔의 커피를 마실 수 있을까? _ 출력할때, 메모리 참조값이 아닌 배열 결과값이 나오기 위해서는 Arrays.toString(how.solution(55000));으로 변환하여야 한다!

---
+++ basic/HowManyCoffee.java
@@ -0,0 +1,17 @@
+package basic;
+
+import java.util.Arrays;
+
+public class HowManyCoffee {
+    public int[] solution (int money) {
+        int pricePerCup = 5500;
+        int cups = money / pricePerCup;
+        int remainingMoney = money % pricePerCup;
+        return new int[] {cups, remainingMoney};
+    }
+
+    public static void main(String[] args) {
+        HowManyCoffee how = new HowManyCoffee();
+        System.out.println(Arrays.toString(how.solution(55000));
+    }
+}
