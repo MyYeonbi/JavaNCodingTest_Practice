c5c418837b7981dc3d12a92e1eb572ea4bde359a
MyYeonbi <yeonbi_kim@icloud.com>
2024-12-31 15:41:28 +0900

    🍬  런타임 오류는 고객이 이용중에 생긴 문제이므로 좋지 않은 오류이다.

--- javaBasicLecture/poly/basic/CastingMain4.java
+++ javaBasicLecture/poly/basic/CastingMain4.java
@@ -8,8 +8,11 @@ public class CastingMain4 {
         Child child1 = (Child) parent1; // 다운캐스팅
         child1.childMethod(); // 문제 없음
 
-        //Parent parent2 = new Parent();
+        /*Parent parent2 = new Parent();
         Child child2 = (Child) parent2;
         child2.childMethod();
+         이건 런타임 오류인데 매우 안좋은 오류이다.
+         왜냐하면 보통 고객이 해당 프로그램을 실행하는 도중에 발생하기 때문이다.
+         */
     }
 }
