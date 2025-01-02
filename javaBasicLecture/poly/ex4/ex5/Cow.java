b1acbc66c00709ab10f2273e89bdb5c12fb8b31f
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-02 17:13:48 +0900

    🎠 Interface도 추상이므로 객체 생성 불가능.

---
+++ javaBasicLecture/poly/ex4/ex5/Cow.java
@@ -0,0 +1,15 @@
+package javaBasicLecture.poly.ex4.ex5;
+
+
+public class Cow implements InterFaceAnimal {
+    @Override
+    public void sound() {
+        System.out.println("음매~");
+
+    }
+
+    @Override
+    public void move() {
+        System.out.println("소가 걸어다닙니다. ");
+    }
+}
---
+++ javaBasicLecture/poly/ex4/ex5/InterfaceMain.java
@@ -0,0 +1,11 @@
+package javaBasicLecture.poly.ex4.ex5;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        InterFaceAnimal interFaceAnimal = new InterFaceAnimal();
+
+
+    }
+}
