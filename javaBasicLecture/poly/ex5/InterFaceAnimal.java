2353f9c03b8237766fe0d7c91bd08b242e1e8cef
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-02 17:27:51 +0900

    🎠 인터페이스는 모든 메서드가 추상 메서드이기 때문에  내부 함수가 들어간 public 메서드를 추가할 수 없다. _ 인터페이스는 인터페이스의 메서드를 반드시 구현하라는 규약을 주는 것이다.

--- javaBasicLecture/poly/ex5/InterFaceAnimal.java
+++ javaBasicLecture/poly/ex5/InterFaceAnimal.java
@@ -3,4 +3,8 @@ package javaBasicLecture.poly.ex5;
 public interface InterFaceAnimal {
     void sound(); //public abstract
     void move(); //public abstract
+
+    /*public void hello(){
+        System.out.println("안녕하세요. 동물입니다. ");
+    }*/
 }
