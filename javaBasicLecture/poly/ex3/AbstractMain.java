63414334e0fe8c6871afba66f7cffeafeecf7911
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-02 15:26:02 +0900

    🐶 추상클래스의 객체 생성을 시도했으나 의도한 대로 객체 생성은 되지 않는다.

---
+++ javaBasicLecture/poly/ex3/AbstractMain.java
@@ -0,0 +1,11 @@
+package javaBasicLecture.poly.ex3;
+
+public class AbstractMain {
+
+    public static void main(String[] args) {
+        //추상클래스 생성 불가
+        AbstractAnimal animal =new AbstractAnimal() {
+            
+        }
+    }
+}
