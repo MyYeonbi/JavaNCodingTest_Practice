f70d8693403a777d328a94340b6dec3e1b3aecce
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-02 15:50:16 +0900

    🐶 내부 메서드들도 모두 추상인 순수한 추상 클래스

---
+++ javaBasicLecture/poly/ex4/Cat.java
@@ -0,0 +1,9 @@
+package javaBasicLecture.poly.ex4;
+
+
+public class Cat extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("냐옹~");
+    }
+}
---
+++ javaBasicLecture/poly/ex4/Cow.java
@@ -0,0 +1,8 @@
+package javaBasicLecture.poly.ex4;
+
+public class Cow extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("음매~");
+    }
+}
