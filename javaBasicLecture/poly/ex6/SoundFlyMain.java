9a001603a40fc4dffa1bbc1d16985c3a2cffb377
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-03 23:19:33 +0900

    🍓 순수추상인 메서드를 내부 함수로 하는 메서드 생성 가능.

---
+++ javaBasicLecture/poly/ex6/SoundFlyMain.java
@@ -0,0 +1,23 @@
+package javaBasicLecture.poly.ex6;
+
+
+
+public class SoundFlyMain {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        Chicken chicken = new Chicken();
+
+        soundAnimal(dog);
+        soundAnimal(bird);
+        soundAnimal(chicken);
+
+    }
+    // AbstractAnimal 사용 가능
+    private static void soundAnimal(AbstractAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+}
