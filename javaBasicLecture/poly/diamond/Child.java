6e0664c3896cd3d45592815f58b4aa55544968cc
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-03 22:42:27 +0900

    🍓 public void methodCommon()  는 인터페이스 두개에 모두 있는 것인데, 자식클래스는 하나만 오버라이딩해도 된다. (interface의 메서드는 내용이 없으므로 어떤것을 쓰든 상관 없다.)

--- javaBasicLecture/poly/diamond/Child.java
+++ javaBasicLecture/poly/diamond/Child.java
@@ -1,4 +1,18 @@
 package javaBasicLecture.poly.diamond;
 
 public class Child implements InterfaceA, InterfaceB {
+    @Override
+    public void methodA() {
+
+    }
+
+    @Override
+    public void methodCommon() {
+
+    }
+
+    @Override
+    public void methodB() {
+
+    }
 }
