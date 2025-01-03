1b7e0c874679b206819300fdd91ed0b3cf8b0b88
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-03 22:50:27 +0900

    🍓 객체는 interfaceB 인스턴스는 child일때, 메모리상 child에 가서 오버라이딩 된것을 우선으로 호출한다.

--- javaBasicLecture/poly/diamond/DiamondMain.java
+++ javaBasicLecture/poly/diamond/DiamondMain.java
@@ -3,5 +3,11 @@ package javaBasicLecture.poly.diamond;
 public class DiamondMain {
     public static void main(String[] args) {
         InterfaceA a = new Child();
+        a.methodA();
+        a.methodCommon();
+
+        InterfaceB b = new Child();
+        b.methodB();
+        b.methodCommon();
     }
 }
