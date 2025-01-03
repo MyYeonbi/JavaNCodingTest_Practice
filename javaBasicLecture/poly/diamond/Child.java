bdb5f6c1a8e276bb87fe9b1c1b81e23c149ca0eb
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-03 22:44:25 +0900

    🍓 결론 : 인터페이스는 다중 구현 가능, 겹치는 메서드 하나만 하면 된다.

--- javaBasicLecture/poly/diamond/Child.java
+++ javaBasicLecture/poly/diamond/Child.java
@@ -3,16 +3,16 @@ package javaBasicLecture.poly.diamond;
 public class Child implements InterfaceA, InterfaceB {
     @Override
     public void methodA() {
-
+        System.out.println("Child methodA");
     }
 
     @Override
     public void methodCommon() {
-
+        System.out.println("Child methodCommonA");
     }
 
     @Override
     public void methodB() {
-
+        System.out.println("Child methodB");
     }
 }
