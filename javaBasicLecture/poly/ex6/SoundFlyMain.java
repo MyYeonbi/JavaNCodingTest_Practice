eb3df5f493f045f4f9485da42ed3003209f69f46
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-03 23:23:11 +0900

    🍓 인터페이스fly를 매개변수와 내부 함수로 넣었을 때, 이 메서드를 활용할 수 있는 것은 해당 인터페이스를 implements한 클래스(bird, chicken)이다.

--- javaBasicLecture/poly/ex6/SoundFlyMain.java
+++ javaBasicLecture/poly/ex6/SoundFlyMain.java
@@ -12,12 +12,23 @@ public class SoundFlyMain {
         soundAnimal(bird);
         soundAnimal(chicken);
 
+        flyAnimal(bird);
+        flyAnimal(chicken);
+
     }
     // AbstractAnimal 사용 가능
     private static void soundAnimal(AbstractAnimal animal) {
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
+    }
+
+    //Fly 인터페이스가 있으면 사용 가능
+    private static void flyAnimal(Fly fly){
+        System.out.println("날기 테스트 시작");
+        fly.fly();
+        System.out.println("날기 테스트 시작");
+
     }
 
 }
