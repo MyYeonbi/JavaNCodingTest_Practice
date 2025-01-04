0576f325b41097c7c78b0dd08f19efd909ba645a
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-04 21:02:38 +0900

    💰 ⭕ PayService 클래스에서 Pay인터페이스를 부모 클래스로 하여 하위의 페이 클래스를 인스턴스로 생성하여 처리 / 'pay(amount)처리도 한번에'

--- javaBasicLecture/poly/q2Poly/PayService.java
+++ javaBasicLecture/poly/q2Poly/PayService.java
@@ -4,18 +4,22 @@ public class PayService {
 
     public void processPay(String option, int amount) {
 
-        boolean result;
+        boolean result = false;
         System.out.println("결제를 시작합니다: option = " + option + ", amount = " + amount);
+        Pay pay;
         if (option.equals("kakao")) {
-            KakaoPay kakaoPay = new KakaoPay();
-            result = kakaoPay.pay(amount);
+            pay = new KakaoPay();
         } else if (option.equals("naver")) {
-            NaverPay naverPay = new NaverPay();
-            result = naverPay.pay(amount);
+            pay = new NaverPay();
         } else {
+            pay = null;
             System.out.println("결제 수단이 없습니다.");
-            result = false;
+
         }
+        if (pay != null) {
+            result = pay.pay(amount);
+        }
+
 
         if (result) {
             System.out.println("결제가 성공했습니다.");
