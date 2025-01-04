bb10be2d18b2d366e072cbff15e68363e9db1c00
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-04 19:04:27 +0900

    ⭕ Sender인터페이스의 sendMessage 메서드의 매개변수는 sout에서 활용해준다.

--- javaBasicLecture/poly/q1/EmailSender.java
+++ javaBasicLecture/poly/q1/EmailSender.java
@@ -3,6 +3,6 @@ package javaBasicLecture.poly.q1;
 public class EmailSender implements Sender {
     @Override
     public void sendMessage(String message) {
-        System.out.println("메일을 발송합니다.");
+        System.out.println("메일을 발송합니다." +message);
     }
 }
--- javaBasicLecture/poly/q1/FaceBookSender.java
+++ javaBasicLecture/poly/q1/FaceBookSender.java
@@ -2,7 +2,7 @@ package javaBasicLecture.poly.q1;
 
 public class FaceBookSender implements Sender {
     @Override
-    public void sendMessage() {
-        System.out.println("페이스북을 발송합니다.");
+    public void sendMessage(String message) {
+        System.out.println("페이스북을 발송합니다." +message);
     }
 }
