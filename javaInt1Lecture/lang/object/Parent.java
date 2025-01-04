3594f0ec9f5f392106218cf909e3d096968799f7
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-05 00:53:58 +0900

    🐤 아무것도 상속 받지 않은 최상위 클래스는 extends Object이 생략된 것이다.

--- javaInt1Lecture/lang/object/Parent.java
+++ javaInt1Lecture/lang/object/Parent.java
@@ -1,7 +1,7 @@
 package javaInt1Lecture.lang.object;
 
 // 부모가 없으면 묵시적으로 Object 클래스를 상속받는다.
-public class Parent {
+public class Parent extends Object {
 
     public void parentMethod() {
         System.out.println("Parent.parentMethod");
