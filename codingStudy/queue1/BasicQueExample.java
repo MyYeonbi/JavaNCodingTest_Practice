50dc56cc57d61f11fd42b2e1959227b1e50f3c05
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-05 16:47:41 +0900

    📖 queue.remove() 괄호 안에는 제거할 요소를 넣는다. queue안에  해당 요소가 있다면, 그 요소가 삭제되고 true를 반환한다.

--- codingStudy/queue1/BasicQueExample.java
+++ codingStudy/queue1/BasicQueExample.java
@@ -13,5 +13,16 @@ public class BasicQueExample {
         System.out.println("제일 처음 값: " + queue.peek()); // 10
         System.out.println("제거된 값 : " + queue.remove()); //10
         System.out.println("큐 상태: " + queue); // [20, 30]
+
+        /* remove()와 peek()는 괄호 안에 아무것도 안 넣었을때는
+        FIFO원칙으로 맨 첫번째 요소가 선택되거나 제거된다.
+        - 괄호 안에 특정 값을 넣으면 :
+        해당 값이 큐 안에 있다면, 그 값을 제거하고 true를 반환한다.
+        해당 값이 큐에 없으면 제거하지 않고 false를 반환한다.
+
+         */
+
+        System.out.println(queue.remove(20)); //true
+        System.out.println(queue); //[30]
     }
 }
