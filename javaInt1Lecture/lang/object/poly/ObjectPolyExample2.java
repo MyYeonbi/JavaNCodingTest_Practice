563a5d90b1daa9afb6386cb25d9aeadc611a7297
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-06 17:02:40 +0900

    :squirrel: Object 인스턴스도 만들 수 있다.

---
+++ javaInt1Lecture/lang/object/poly/ObjectPolyExample2.java
@@ -0,0 +1,9 @@
+package javaInt1Lecture.lang.object.poly;
+
+public class ObjectPolyExample2 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+        Object o = new Object(); //Object 인스턴스도 만들 수 있다. 
+    }
+}
