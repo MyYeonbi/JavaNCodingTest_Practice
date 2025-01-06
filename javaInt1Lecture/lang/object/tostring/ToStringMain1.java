264990699b1f694c2d822551380816f6791e1e46
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-06 17:19:09 +0900

    :squirrel: object.toString()를 출력하면 '클래스 정보 + 메모리주소'가 출력됨._toString()설명.

---
+++ javaInt1Lecture/lang/object/tostring/ToStringMain1.java
@@ -0,0 +1,13 @@
+package javaInt1Lecture.lang.object.tostring;
+
+public class ToStringMain1 {
+    public static void main(String[] args) {
+        Object object = new Object();
+        String string = object.toString();
+
+        //toString()반환값 출력
+        System.out.println(string);
+
+
+    }
+}
