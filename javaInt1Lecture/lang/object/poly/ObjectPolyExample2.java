dcf7c6ef06ee0ed363f2438ada46b94050709afb
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-06 17:08:34 +0900

    :squirrel: Object 타입을 사용한 배열은 세상의 모든 객체를 담을수 있는 배열이다.

--- javaInt1Lecture/lang/object/poly/ObjectPolyExample2.java
+++ javaInt1Lecture/lang/object/poly/ObjectPolyExample2.java
@@ -8,6 +8,7 @@ public class ObjectPolyExample2 {
         Car car = new Car();
         Object object = new Object(); //Object 인스턴스도 만들 수 있다.
 
+        //Object 타입을 사용한 배열은 세상의 모든 객체를 담을수 있는 배열이다.
         Object [] objects = {dog,car,object};
 
         size(objects);
