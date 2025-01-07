7997375237c09ab4cafc907c9b04635b4ea20186
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-07 18:24:30 +0900

    💐  b = new ImmutableAddress("부산");_  a 말고 b에 새로운 객체를 생성하여 value 값을 다시 설정할 수 있다.

--- javaInt1Lecture/lang/immutable/address/RefMain2.java
+++ javaInt1Lecture/lang/immutable/address/RefMain2.java
@@ -4,12 +4,13 @@ public class RefMain2 {
 
     public static void main(String[] args) {
 
-        ImmutableAddress a = new Address("서울");
+        ImmutableAddress a = new ImmutableAddress("서울");
         ImmutableAddress b = a; // 참조값 대입을 막을수 있는 방법은 없다.
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        b.setValue("부산"); //b의 값을 부산으로 변경해야함.
+       // b.setValue("부산"); //b의 값을 부산으로 변경해야함.
+        b = new ImmutableAddress("부산");
         System.out.println("---- 부산 -> b ----");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
