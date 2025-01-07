fff9d12b25bab7a9fe259ec5b1186c6ebb1c6ba2
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-07 20:29:10 +0900

    🌹 사이드 이펙트가 발생한 클래스

--- javaInt1Lecture/lang/immutable/change/ImmutableMain2.java
+++ javaInt1Lecture/lang/immutable/change/ImmutableMain2.java
@@ -4,7 +4,7 @@ public class ImmutableMain2 {
 
     public static void main(String[] args) {
         ImmutableObj obj1 =new ImmutableObj(10);
-    obj1.add(20); // 꼭 반환값을 반환해야지 이것이 적용이 된다. 
+    obj1.add(20); // 꼭 반환값을 반환해야지 이것이 적용이 된다.
 
         // 계산 이후에도 기존값과 신규값 모두 확인 가능
         System.out.println("obj1 = " + obj1.getValue());
