ffe18384ac155cb3ef1b178567abed4030487a0e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-07 16:39:09 +0900

    🎋 (코테 복습)_ 6조각 한판인 피자와 n명이 있을때 n명이 공통적으로 받는 피자 조각수가 같아야 할 때, 피자는 몇 판 필요할까? _ 최소공배수

--- codingSelfStudy/division/GcdLcm.java
+++ codingSelfStudy/division/GcdLcm.java
@@ -1,6 +1,12 @@
 package codingSelfStudy.division;
-
+/* 피자 한판에 6조각이 있다. 특정인원 n 를 입력받아 이들에게 최소 1조각씩 나눠주기 위해
+ 최소한의 피자 판 수를 계산한다.
+ n명은 모두 공통된 조각수를 배분 받아야 한다. 
+* */
 public class GcdLcm {
+
+    /* 피자조각수와 사람수의 최소공배수를 계산하여,
+    이를 피자 한 판의 조각수(6)로 나눠 피자 판수를 구한다. */
     public int solution(int n) {
         int gcd = gcd(6,n); //최대공약수 구하기
         int lcm = (6*n)/gcd; //최소공배수 구하기
