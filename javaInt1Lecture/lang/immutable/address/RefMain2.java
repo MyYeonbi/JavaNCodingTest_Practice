78f46bd93bf02290584dd87429386b03b4750c19
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-07 18:22:53 +0900

    💐 ❗  b.setValue("부산");으로 value값을 다시 설정 하려하자 오류가 난다. 이유는 value필드를 final로 해두어서.

--- javaInt1Lecture/lang/immutable/address/RefMain2.java
+++ javaInt1Lecture/lang/immutable/address/RefMain2.java
@@ -5,7 +5,7 @@ public class RefMain2 {
     public static void main(String[] args) {
 
         ImmutableAddress a = new Address("서울");
-        ImmutableAddress b = a; // 참조값 대입을 막을수 있는 방법은 없다. 
+        ImmutableAddress b = a; // 참조값 대입을 막을수 있는 방법은 없다.
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
