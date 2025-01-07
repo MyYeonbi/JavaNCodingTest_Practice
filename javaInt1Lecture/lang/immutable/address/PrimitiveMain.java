554dc6040d382f94e7d27e0972b6e380fb095e2c
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-07 11:41:18 +0900

    💐 a와 b는 같은 10이여도 하나의 값을 공유하지 않는다. 그래서 b에 다른 값을 넣어도 a는 영향을 받지 않는다.

--- javaInt1Lecture/lang/immutable/address/PrimitiveMain.java
+++ javaInt1Lecture/lang/immutable/address/PrimitiveMain.java
@@ -12,5 +12,10 @@ public class PrimitiveMain {
         System.out.println("--- 20 -> b -----");
         System.out.println("a = " + a);
         System.out.println("b = "+ b);
+        /* 이렇게 나온건 b는 a의 값을 복사해서 대입했었기 때문이다.
+        그래서 동일한 10이 아닌것이다. 하나의 값을 공유하지 않는다는 말이다.
+        b= 20을 대입하였을때 서로 공유하지 않기 때문에 a는 여전히 10인것. 
+         */
+
     }
 }
