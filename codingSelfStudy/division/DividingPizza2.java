c48890e69341f3832a5b5910dabb9f9fe4546088
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-07 16:02:10 +0900

    🎋 (코테 복습)_ 조각수와 사람수가 주어질때, n명이 최소 1조각씩 받도록 하기 위해 필요한 피자 판수

--- codingSelfStudy/division/DividingPizza2.java
+++ codingSelfStudy/division/DividingPizza2.java
@@ -1,6 +1,9 @@
 package codingSelfStudy.division;
 
 public class DividingPizza2 {
+    /* 피자 한판의 조각수(slice)와 총 사람 수(n)를 입력받아
+     n명이 최소 1조각씩 받도록 하기위해 필요한 피자 판수 */
+
     public int solution(int slice, int n) {
         if (slice < 2 || slice > 10) {
             throw new IllegalArgumentException("slice는 2 이상 10 이하의 값이어야 한다.");
