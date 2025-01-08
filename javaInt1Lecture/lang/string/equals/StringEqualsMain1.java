f0bee7e3f48568e2e94e5e797f1857528b14aaac
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-08 12:19:27 +0900

    🍭 str1 == str2 (참조값이 같은지 확인) false / str1.equals(str2)(String클래스 안의 value값이 같은지 확인) true

---
+++ javaInt1Lecture/lang/string/equals/StringEqualsMain1.java
@@ -0,0 +1,13 @@
+package javaInt1Lecture.lang.string.equals;
+
+public class StringEqualsMain1 {
+
+    public static void main(String[] args) {
+        String str1 = new String("hello");
+        String str2 = new String("hello");
+        System.out.println("new String() == 비교" + (str1 == str2));
+        System.out.println("new String() equals 비교:" + str1.equals(str2));
+
+
+    }
+}
