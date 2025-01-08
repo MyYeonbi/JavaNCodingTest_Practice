ce813b2895eedafe195682284be9d955ef71f96e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-08 20:20:16 +0900

    🍭 매서드 체이닝 설명 _ add() 메서드를 호출하면 ValueAdder인스턴스 자신의 참조값이 반환된다. 이 반환된 참조값을 변수에 담아두지 않아도 된다. 대신에 반환된 참조값을 즉시 사용해서 바로 메서드를 호출할 수 있다.

---
+++ javaInt1Lecture/lang/string/chaining/ValueAdder.java
@@ -0,0 +1,15 @@
+package javaInt1Lecture.lang.string.chaining;
+
+public class ValueAdder {
+
+    private int value;
+
+    public ValueAdder add(int addValue) {
+        value += addValue;
+        return this;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
