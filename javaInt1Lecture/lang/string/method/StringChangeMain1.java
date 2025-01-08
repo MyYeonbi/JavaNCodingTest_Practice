c61f265ea459a139f6aa9226a084bf3d99c7bda8
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-08 15:30:37 +0900

    🍭 문자열 변형2_ 대/소문자 변환, 공백 제거, 앞/뒤 공백 제거.

--- javaInt1Lecture/lang/string/method/StringChangeMain1.java
+++ javaInt1Lecture/lang/string/method/StringChangeMain1.java
@@ -11,6 +11,6 @@ public class StringChangeMain1 {
         System.out.println("문자열 결합: " + str.concat("!!!"));
         System.out.println("'Java'를 'World'로 대체 : " + str.replace("Java", "World"));
         System.out.println("첫 번째 'Java'를 'World'로 대체: " + str.replaceFirst("Java","World"));
-        
+
     }
 }
