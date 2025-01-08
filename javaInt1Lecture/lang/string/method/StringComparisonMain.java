eae7967ae3c62c47893cd2fae45bb9cfec3ab0a8
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-08 14:34:09 +0900

    🍭 .compareTo메서드와 .startsWith() 메서드 사용해보기.

--- javaInt1Lecture/lang/string/method/StringComparisonMain.java
+++ javaInt1Lecture/lang/string/method/StringComparisonMain.java
@@ -3,10 +3,16 @@ package javaInt1Lecture.lang.string.method;
 public class StringComparisonMain {
     public static void main(String[] args) {
         String str1 = "Hello, Java!"; // 대문자 일부 있음
-        String str2 = "hello, java!"; // 모두 소무낮
+        String str2 = "hello, java!"; // 모두 소문자
         String str3 = "Hello, World!";
 
         System.out.println("str1 equals str2: "+ str1.equals(str2));
         System.out.println("str2 equals str3: "+ str2.equalsIgnoreCase(str2));
+
+        System.out.println("'b' compareTo 'a': "+ "b".compareTo("a"));
+        System.out.println("str1 compareTo str3: "+ str1.compareTo(str3));
+        System.out.println("str1 compareToIgnoreCase str2: "+ str1.compareToIgnoreCase(str2));
+
+        System.out.println("str1 starts with 'Hello': ")+ str1.startsWith("Hello");
     }
 }
