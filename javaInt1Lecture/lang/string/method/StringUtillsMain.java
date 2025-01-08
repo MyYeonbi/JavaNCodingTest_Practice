2a0d626a3647d85fc4532f287702368aaa0eeedd
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-08 18:42:34 +0900

    🍭 valueOf 메서드 사용

---
+++ javaInt1Lecture/lang/string/method/StringUtillsMain.java
@@ -0,0 +1,20 @@
+package javaInt1Lecture.lang.string.method;
+
+public class StringUtillsMain {
+    public static void main(String[] args) {
+        int num = 100;
+        boolean bool = true;
+        Object obj = new Object();
+        String str = "Hello, Java!";
+
+        //valueOf 메서드
+        String numString = String.valueOf(num);
+        System.out.println("숫자의 문자열 값:" + numString);
+        String boolString = String.valueOf(bool);
+        System.out.println("불리언의 문자열 값: " + boolString);
+        String objString = String.valueOf(obj);
+        System.out.println("객체의 문자열 값: " +  objString);
+        
+
+    }
+}
