7bb15c7222bc187d08b9f6b6f259a005b2337222
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-08 10:46:43 +0900

    🍭 int, double, boolean은 기본형 타입이다.  String은 참조형이다. 그래서 객체 생성이 가능하다.

---
+++ javaInt1Lecture/lang/string/StringBasicMain.java
@@ -0,0 +1,11 @@
+package javaInt1Lecture.lang.string;
+
+public class StringBasicMain {
+
+    public static void main(String[] args) {
+        //int, double, boolean은 기본형
+        // String은 참조형이다. 그래서 객체 생성이 가능하다.
+        String str1  = "hello";
+        String str2 = new String("hello");
+    }
+}
