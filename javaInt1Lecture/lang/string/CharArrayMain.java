c046336dd23e0e014a312e4f334065be744593cc
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-08 10:41:31 +0900

    🍭 char은 문자 하나만 담을수 있다. 여러개 담고 싶다면 char[]  배열 형태로 담아준다.

---
+++ javaInt1Lecture/lang/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package javaInt1Lecture.lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char a = 'b';
+        System.out.println("a = " + a);
+
+        char[] charArr =  new char[] { 'a', 'b', 'c' };
+        System.out.println(charArr);
+    }
+}
