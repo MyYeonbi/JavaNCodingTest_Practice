27ad0c2a5fa3d85dc185413e9df1d053806c614c
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 18:34:49 +0900

    🍑 빨간 경고 메시지가 뜨는 이유는, 이러한 래퍼클래스는 미래에 삭제 예정이고 대신에 valueOf()를 사용 권장한다는 의미이다. 문제 없이 실행되기는 한다.

--- javaInt1Lecture/lang/wrapper/WrapperClassMain.java
+++ javaInt1Lecture/lang/wrapper/WrapperClassMain.java
@@ -4,8 +4,8 @@ public class WrapperClassMain {
 
     public static void main(String[] args) {
         Integer newInteger = new Integer(10);
-        /*빨간 경고 메시지가 뜨는 이유는,
-        * 미래에 삭제 예정, 대신에 valueOf()를 사용 */
+        // /*빨간 경고 메시지가 뜨는 이유는,
+        //        * 미래에 삭제 예정, 대신에 valueOf()를 사용 */
         System.out.println("newInteger = " + newInteger);
 
 
