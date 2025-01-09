7c2ef2a083ddc49bf601e2baa2c393376e5d87a5
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 18:33:27 +0900

    🍑 래퍼 클래스(Wrapper Class)는 기본형(primitive type)을 객체(object)로 다룰 수 있도록 설계된 클래스이다

---
+++ javaInt1Lecture/lang/wrapper/WrapperClassMain.java
@@ -0,0 +1,13 @@
+package javaInt1Lecture.lang.wrapper;
+
+public class WrapperClassMain {
+
+    public static void main(String[] args) {
+        Integer newInteger = new Integer(10);
+        /*빨간 경고 메시지가 뜨는 이유는,
+        * 미래에 삭제 예정, 대신에 valueOf()를 사용 */
+        System.out.println("newInteger = " + newInteger);
+
+
+    }
+}
