f05d5ae08c2f2ac206b67b4bfe66b828b57eb942
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 21:42:03 +0900

    🍉 Class.forName("java.lang.String");  : 3. 문자열로 조회

--- javaInt1Lecture/lang/clazz/ClassMataMain.java
+++ javaInt1Lecture/lang/clazz/ClassMataMain.java
@@ -5,5 +5,6 @@ public class ClassMataMain {
         //Class 조회
         Class clazz = String.class; //1. 클래스에서 조회
         Class clazz1 = new String().getClass(); //2. 인스턴스에서 조회
+        Class clazz2 = Class.forName("java.lang.String"); //3. 문자열로 조회
     }
 }
