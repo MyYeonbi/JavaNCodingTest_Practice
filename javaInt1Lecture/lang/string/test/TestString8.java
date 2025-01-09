9b69d58ab4d3efbae99b0fe216add32fbfeeaa2a
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 15:47:59 +0900

    🍭 문자열 대체하기 : .replace("수정 전 문구", "바꿀 문구");

---
+++ javaInt1Lecture/lang/string/test/TestString8.java
@@ -0,0 +1,9 @@
+package javaInt1Lecture.lang.string.test;
+
+public class TestString8 {
+    public static void main(String[] args) {
+        String input = "Hello java spring jpa java";
+        String replace = input.replace("java", "jvm");
+        System.out.println("replace = " + replace);
+    }
+}
