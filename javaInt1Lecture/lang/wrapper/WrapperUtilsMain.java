8c452c5facda507682e83e21918b0348a61a8f68
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 20:36:24 +0900

    🍑 객체 간의 크기 비교 : .compareTo();

--- javaInt1Lecture/lang/wrapper/WrapperUtilsMain.java
+++ javaInt1Lecture/lang/wrapper/WrapperUtilsMain.java
@@ -5,5 +5,9 @@ public class WrapperUtilsMain {
         Integer i1 = Integer.valueOf(10);//숫자, 래퍼 객체 변환
         Integer i2 = Integer.valueOf(10);//문자열, 래퍼 객체로 변환
         int intValue = Integer.parseInt("10");// 문자열 전용, 기본형 변환
+
+        //비교
+        int compareResult = i1.compareTo(20);
+        System.out.println("compareResult = " + compareResult);
     }
 }
