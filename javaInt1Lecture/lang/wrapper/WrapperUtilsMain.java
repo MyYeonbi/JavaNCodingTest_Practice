b3a5ce63593f1ab12890771e4d93033fd7d4a8b7
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 20:33:32 +0900

    🍑 기본형 int -> Integer : Integer.valueOf   //   String -> int : Integer.parseInt();

---
+++ javaInt1Lecture/lang/wrapper/WrapperUtilsMain.java
@@ -0,0 +1,9 @@
+package javaInt1Lecture.lang.wrapper;
+
+public class WrapperUtilsMain {
+    public static void main(String[] args) {
+        Integer i1 = Integer.valueOf(10);//숫자, 래퍼 객체 변환
+        Integer i2 = Integer.valueOf(10);//문자열, 래퍼 객체로 변환
+        int intValue = Integer.parseInt("10");// 문자열 전용, 기본형 변환
+    }
+}
