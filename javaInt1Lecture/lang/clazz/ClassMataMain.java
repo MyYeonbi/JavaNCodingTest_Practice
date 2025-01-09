0c0364fa6fb1d914804e9c9c80a8c22cbe362623
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 21:40:39 +0900

    🍉2. 인스턴스에서 조회 : new String().getClass();

--- javaInt1Lecture/lang/clazz/ClassMataMain.java
+++ javaInt1Lecture/lang/clazz/ClassMataMain.java
@@ -4,5 +4,6 @@ public class ClassMataMain {
     public static void main(String[] args) {
         //Class 조회
         Class clazz = String.class; //1. 클래스에서 조회
+        Class clazz1 = new String().getClass(); //2. 인스턴스에서 조회
     }
 }
