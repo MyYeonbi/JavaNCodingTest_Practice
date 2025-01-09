124955034ac960ce6587a378b63fcc3d86a76f82
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 15:44:02 +0900

    🍭  문자열 앞뒤 공백 없애기 : .trim();

---
+++ javaInt1Lecture/lang/string/test/TestString7.java
@@ -0,0 +1,9 @@
+package javaInt1Lecture.lang.string.test;
+
+public class TestString7 {
+    public static void main(String[] args) {
+        String original = "     Hello World   ";
+        String trimmed = original.trim();
+        System.out.println("trimmed: " + trimmed);
+    }
+}
