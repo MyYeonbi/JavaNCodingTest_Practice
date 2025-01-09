858b78e5c29dd21c28f173b18f0e0eeb3f307130
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 18:21:56 +0900

    🍑  두 개의 메서드는 return 값을 지정된 숫자이냐 Nul값으로 설정하냐의 차이가 있다. 이는 int는 기본형이라 null값이 아니라 반드시 값이 존재해야 하는 특징이 있고, Integer은 참조형이라 null값을 가질  수 있다는 걸 보여주기 위해서 만든 메서드들이다.

--- javaInt1Lecture/lang/wrapper/MyIntegerNullMain1.java
+++ javaInt1Lecture/lang/wrapper/MyIntegerNullMain1.java
@@ -19,3 +19,8 @@ public class MyIntegerNullMain1 {
         return null;
     }
 }
+/* 기본형은 항상 값이 존재해야 한다. 숫자의 경우 0,-1 같은 값이라도 항상 존재해야 한다.
+반면에 객체인 참조형은 값이 없다는 null을 사용할 수 있다.
+물론 null값을 반환하는 경우 잘못하면 NullPointerException이 발생할 수 있기 때문에 주의해야 한다.
+*
+* */
