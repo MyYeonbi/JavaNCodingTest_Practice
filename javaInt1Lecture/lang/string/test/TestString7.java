406cf060f1524cff0a14d73380a4269663694378
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 15:44:59 +0900

    🍭 문자열 앞뒤 공백 없애는 메서드는 .strip()도 가능!

--- javaInt1Lecture/lang/string/test/TestString7.java
+++ javaInt1Lecture/lang/string/test/TestString7.java
@@ -3,7 +3,8 @@ package javaInt1Lecture.lang.string.test;
 public class TestString7 {
     public static void main(String[] args) {
         String original = "     Hello World   ";
-        String trimmed = original.trim();
+        String trimmed = original.strip();
+
         System.out.println("trimmed: " + trimmed);
     }
 }
