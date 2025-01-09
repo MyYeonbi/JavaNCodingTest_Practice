ebf091572377c332eefd0ef5ce93f00e1485eb9e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 21:43:21 +0900

    🍉 조회하려는 클래스에 반드시 throws Exception를 입력해줘야 한다.

--- javaInt1Lecture/lang/clazz/ClassMataMain.java
+++ javaInt1Lecture/lang/clazz/ClassMataMain.java
@@ -1,7 +1,7 @@
 package javaInt1Lecture.lang.clazz;
 
 public class ClassMataMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //Class 조회
         Class clazz = String.class; //1. 클래스에서 조회
         Class clazz1 = new String().getClass(); //2. 인스턴스에서 조회
