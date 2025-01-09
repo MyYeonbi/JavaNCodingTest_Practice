7de07aa6aa97eb64be0156405ddafdc877a22f35
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-09 18:37:56 +0900

    🍑 Integer.valueOf();를 사용 권장하는 이유는, -128 ~ 127 자주 사용하는 숫자 값 재사용할  수 있게 풀로 저장되어 있다. 또한  불변 객체이다.

--- javaInt1Lecture/lang/wrapper/WrapperClassMain.java
+++ javaInt1Lecture/lang/wrapper/WrapperClassMain.java
@@ -8,6 +8,8 @@ public class WrapperClassMain {
         //        * 미래에 삭제 예정, 대신에 valueOf()를 사용 */
         System.out.println("newInteger = " + newInteger);
 
+        Integer integerObj = Integer.valueOf(10);//-128 ~ 127 자주 사용하는 숫자 값 재사용. 불변
+
 
     }
 }
