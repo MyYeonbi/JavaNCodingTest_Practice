03352cfd2e89bec822a866ef1c5d5e0c88f78718
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 23:36:30 +0900

    ❄️ 반올림 및 정밀도 메서드_ Math.ceil() : 올림

--- javaInt1Lecture/lang/math/MathMain.java
+++ javaInt1Lecture/lang/math/MathMain.java
@@ -7,5 +7,8 @@ public class MathMain {
         System.out.println("min(10, 20)" + Math.min(10, 20)); // 최소값.
         System.out.println("abs(-10)" + Math.abs(-10)); // 절대값
 
+        // 반올림 및 정밀도 메서드
+        System.out.println("ceil(2.1):" + Math.ceil(2.1)); // 올림
+
     }
 }
