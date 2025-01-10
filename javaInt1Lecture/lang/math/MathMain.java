5634791b6c4dde253029bd520c528bc2e4f16160
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 23:33:50 +0900

    ❄️ Math.max : 최대값 // Math.min : 최소값.

--- javaInt1Lecture/lang/math/MathMain.java
+++ javaInt1Lecture/lang/math/MathMain.java
@@ -3,6 +3,8 @@ package javaInt1Lecture.lang.math;
 public class MathMain {
     public static void main(String[] args) {
         // 기본 연산 메서드
-        
+        System.out.println("max(10, 20)" + Math.max(10, 20)); // 최대값.
+        System.out.println("min(10, 20)" + Math.min(10, 20)); // 최소값.
+
     }
 }
