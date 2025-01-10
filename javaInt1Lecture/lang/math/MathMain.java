3f47387729523883e75ae0a22fcc8e9c213a0268
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 23:34:56 +0900

    ❄️ Math.abs() : 절대값

--- javaInt1Lecture/lang/math/MathMain.java
+++ javaInt1Lecture/lang/math/MathMain.java
@@ -5,6 +5,7 @@ public class MathMain {
         // 기본 연산 메서드
         System.out.println("max(10, 20)" + Math.max(10, 20)); // 최대값.
         System.out.println("min(10, 20)" + Math.min(10, 20)); // 최소값.
+        System.out.println("abs(-10)" + Math.abs(-10)); // 절대값
 
     }
 }
