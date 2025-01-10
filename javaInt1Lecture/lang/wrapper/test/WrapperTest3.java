71332d81af9df6c444f59f1371e352ad7e7de7a6
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-11 00:55:13 +0900

    ❄️ Integer.valueOf()와 Integer.parseInt() 공통점과 차이점을 각주로 정리.

--- javaInt1Lecture/lang/wrapper/test/WrapperTest3.java
+++ javaInt1Lecture/lang/wrapper/test/WrapperTest3.java
@@ -7,6 +7,18 @@ public class WrapperTest3 {
         //String -> Integer
         Integer integer1 = Integer.valueOf(str);
 
+        /*Integer.valueOf()와 Integer.parseInt()는 둘 다 문자열을 숫자로 변환하는 데 사용되지만,
+        반환 타입과 사용 목적이 다르다.
+        1. 공통점 :
+        •	둘 다 문자열에 포함된 숫자 데이터를 정수(int)로 변환할 때 사용된다.
+	    •	문자열이 숫자가 아닌 경우, 둘 다 NumberFormatException을 던진다.
+
+	    2. 차이점 :
+	    Integer.valueOf() : 문자열을 Integer 객체로 변환
+	    Integer.parseInt() : 문자열을 기본형 int로 변환
+
+        */
+
         System.out.println();
     }
 }
