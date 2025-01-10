36c93917d12ba79f0b48b072ad89a7183404253b
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 23:59:00 +0900

    ❄️ Random random = new Random(씨드값) ; : 씨드가 같으면 random의 결과가 같다.

--- javaInt1Lecture/lang/math/RandomMain.java
+++ javaInt1Lecture/lang/math/RandomMain.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class RandomMain {
 
     public static void main(String[] args) {
-        Random random = new Random();
+       // Random random = new Random();
+        Random random = new Random(1);// seed가 같으면 Random의 결과가 같다.
 
         int randomInt = random.nextInt();
         System.out.println("randomInt: " + randomInt);
