e32e51d4aae75fd33698405797a1dd4d7f11b4c5
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 22:06:12 +0900

    🌃 System.getenv() : 환경 변수를 읽는다.

--- javaInt1Lecture/lang/system/SystemMain.java
+++ javaInt1Lecture/lang/system/SystemMain.java
@@ -1,5 +1,7 @@
 package javaInt1Lecture.lang.system;
 
+import java.util.Map;
+
 public class SystemMain {
     public static void main(String[] args) {
         // 현재 시간(밀리초)를 가져온다.
@@ -7,6 +9,10 @@ public class SystemMain {
         System.out.println("Current time: " + currentTimeMillis);
 
         // 현재 시간(나노초)를 가져온다.
-        long currentNanoTime = System.nanoTime();
+        long currentTimeNano = System.nanoTime();
+        System.out.println("Current time nanos: " + currentTimeNano);
+
+        // 환경 변수를 읽는다.
+        System.out.println("getenv = " + System.getenv());
     }
 }
