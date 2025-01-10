af0166d3f27aa66e4496d6385123df1bcbf3eaca
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 23:20:16 +0900

    🌃  System.arraycopy : 배열을 고속으로 복사한다.

--- javaInt1Lecture/lang/system/SystemMain.java
+++ javaInt1Lecture/lang/system/SystemMain.java
@@ -18,5 +18,14 @@ public class SystemMain {
         // 시스템 속성을 읽는다.
         System.out.println("proprerties = " + System.getProperties());
         System.out.println("Java version = " + System.getProperty("java.version"));
+
+        // 배열을 고속으로 복사한다.
+        char[] originalArray = {'h','e','l','l','o'};
+        char[] copiedArray = new char[5];
+        System.arraycopy(originalArray, 0, copiedArray, 0, originalArray.length);
+
+        // 배열 출력
+        System.out.println("copiedArray = " + copiedArray);
+
     }
 }
