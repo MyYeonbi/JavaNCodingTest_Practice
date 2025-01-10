f4a5632a90934d0c52f11e83c3a5dbaddee686dc
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 23:59:57 +0900

    ❄️ 씨드가 같으면 반복 실행해도 값이 그대로이다

--- javaInt1Lecture/lang/math/RandomMain.java
+++ javaInt1Lecture/lang/math/RandomMain.java
@@ -6,7 +6,7 @@ public class RandomMain {
 
     public static void main(String[] args) {
        // Random random = new Random();
-        Random random = new Random(1);// seed가 같으면 Random의 결과가 같다.
+        Random random = new Random(2);// seed가 같으면 Random의 결과가 같다.
 
         int randomInt = random.nextInt();
         System.out.println("randomInt: " + randomInt);
