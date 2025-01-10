ab6f213c34983966273fe39b2904cb882d2a7e29
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-10 22:00:58 +0900

    🌃 <자바 중급_ 섹션5-3 System클래스 >

---
+++ javaInt1Lecture/lang/system/SystemMain.java
@@ -0,0 +1,8 @@
+package javaInt1Lecture.lang.system;
+
+public class SystemMain {
+    public static void main(String[] args) {
+        // 현재 시간(밀리초)를 가져온다.
+        long currentTimeMillis = System.currentTimeMillis();
+    }
+}
