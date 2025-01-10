7cba24c26fc4143363622c667a7d9698611eb994
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-11 00:52:43 +0900

    🍞 Integer.valueOf() : 문자열을 Integer(래퍼클래스 객체)로 변환

---
+++ javaInt1Lecture/lang/wrapper/test/WrapperTest3.java
@@ -0,0 +1,12 @@
+package javaInt1Lecture.lang.wrapper.test;
+
+public class WrapperTest3 {
+    public static void main(String[] args) {
+        String str = "100";
+
+        //String -> Integer
+        Integer integer1 = Integer.valueOf(str);
+
+        System.out.println();
+    }
+}
