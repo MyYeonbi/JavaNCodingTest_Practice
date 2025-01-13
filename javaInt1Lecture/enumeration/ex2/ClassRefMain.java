90bc52e3a28e13f7c25397c4a71d5ea1a87ec86f
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-13 15:36:29 +0900

    🐠 이 방식은 열거형(enumeration) 대신 상수를 정의하고 객체로 관리하는 방법으로, 각 상수를 고유한 객체로 다룰 수 있다.

--- javaInt1Lecture/enumeration/ex2/ClassRefMain.java
+++ javaInt1Lecture/enumeration/ex2/ClassRefMain.java
@@ -8,4 +8,8 @@ public class ClassRefMain {
         System.out.println("class GOLD = "+ ClassGrade.GOLD.getClass());
         System.out.println("class DIAMOND = "+ ClassGrade.DIAMOND.getClass());
     }
-}
+}/*코드의 핵심 과정 요약
+	1.	ClassGrade 클래스가 로드될 때, BASIC, GOLD, DIAMOND 상수 필드가 ClassGrade 객체로 각각 초기화된다.
+	2.	ClassRefMain의 main 메서드에서 각 상수 객체의 클래스 타입 정보를 출력한다.
+	3.	출력 결과를 통해 모든 상수 객체는 동일하게 ClassGrade 클래스의 인스턴스임을 알 수 있다.
+*/
