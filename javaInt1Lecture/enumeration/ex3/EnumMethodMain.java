a88286ae5752ae285d6d31b795909076ba89b62e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-13 18:39:08 +0900

    🐙 1. 모든 ENUM 변환 메서드 : Grade[] vlaues = Grade.values();

--- javaInt1Lecture/enumeration/ex3/EnumMethodMain.java
+++ javaInt1Lecture/enumeration/ex3/EnumMethodMain.java
@@ -1,4 +1,11 @@
 package javaInt1Lecture.enumeration.ex3;
 
+import java.util.Arrays;
+
 public class EnumMethodMain {
+    public static void main(String[] args) {
+        // 모든 ENUM 변환
+        Grade[] values = Grade.values();
+        System.out.println("values = " + Arrays.toString(values));
+    }
 }
