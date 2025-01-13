55a9dc5b0200e357ba4e98721e8a90237e582a99
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-13 17:59:05 +0900

    ❌ DiscountService 클래스의 discount 메서드가 ClassGrade 타입을 인수로 받는데, Grade.BASIC 등 Grade 타입의 열거형 상수를 전달했기 때문에 생긴 오류_ 해결 과정은 이슈란 ✨

---
+++ javaInt1Lecture/enumeration/ex3/DiscountService.java
@@ -0,0 +1,22 @@
+package javaInt1Lecture.enumeration.ex3;
+
+import javaInt1Lecture.enumeration.ex2.ClassGrade;
+
+public class DiscountService {
+
+   public int discount(ClassGrade classGrade, int price) {
+       int discountPercent = 0;
+
+       if (classGrade == ClassGrade.BASIC) {
+           discountPercent = 10;
+       } else if (classGrade == classGrade.GOLD) {
+           discountPercent = 20;
+       } else if (classGrade == classGrade.DIAMOND) {
+           discountPercent = 30;
+       } else {
+           System.out.println("할인X");
+       }
+       return price * discountPercent / 100;
+
+   }
+}
