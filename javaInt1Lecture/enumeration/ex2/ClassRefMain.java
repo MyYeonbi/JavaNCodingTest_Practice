9bcb3b41a56d78d7fb8269b5ee4f650beb48032c
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-13 15:33:48 +0900

    🐠 ClassGrade.BASIC.getClass(): BASIC 객체의 런타임 클래스 정보를 반환.

--- javaInt1Lecture/enumeration/ex2/ClassRefMain.java
+++ javaInt1Lecture/enumeration/ex2/ClassRefMain.java
@@ -3,6 +3,7 @@ package javaInt1Lecture.enumeration.ex2;
 public class ClassRefMain {
 
     public static void main(String[] args) {
+        //ClassGrade.BASIC.getClass(): BASIC 객체의 런타임 클래스 정보를 반환.
         System.out.println("class BASIC = "+ ClassGrade.BASIC.getClass());
         System.out.println("class GOLD = "+ ClassGrade.GOLD.getClass());
         System.out.println("class DIAMOND = "+ ClassGrade.DIAMOND.getClass());
