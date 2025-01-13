4e841b0be969e0fa8ca381b27a5a6be087ec7ee7
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-13 16:59:08 +0900

    🐠 	•문제: ClassGrade 객체는 상수(BASIC, GOLD, DIAMOND)로 선언되어 있어야만 의도한 대로 동작한다. 그러나 위 코드처럼 new ClassGrade()로 새 객체를 생성하면, 이 객체는 BASIC, GOLD, DIAMOND 중 어느 것도 아닌 새로운 객체가 된다. 	• 결과: DiscountService의 discount 메서드에서 할인X가 출력되고, 할인 금액이 계산되지 않는다.

--- javaInt1Lecture/enumeration/ex2/ClassGradeEx2.java
+++ javaInt1Lecture/enumeration/ex2/ClassGradeEx2.java
@@ -4,6 +4,9 @@ public class ClassGradeEx2 {
     public static void main(String[] args) {
         int price = 10000;
 
+/*	•	문제: ClassGrade 객체는 상수(BASIC, GOLD, DIAMOND)로 선언되어 있어야만 의도한 대로 동작합니다. 그러나 위 코드처럼 new ClassGrade()로 새 객체를 생성하면, 이 객체는 BASIC, GOLD, DIAMOND 중 어느 것도 아닌 새로운 객체가 됩니다.
+	•	결과: DiscountService의 discount 메서드에서 할인X가 출력되고, 할인 금액이 계산되지 않습니다.
+*/
         DiscountService discountService = new DiscountService();
         ClassGrade classGrade = new ClassGrade();
         int result = discountService.discount(classGrade, price);
