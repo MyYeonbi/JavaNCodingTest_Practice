ae8bb06891bd9b3afb1cd77475da73c38edc88c2
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-15 23:36:02 +0900

    🐴 HttpStatus enum 생성

---
+++ javaInt1Lecture/enumeration/http/HttpStatus.java
@@ -0,0 +1,15 @@
+package javaInt1Lecture.enumeration.http;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
