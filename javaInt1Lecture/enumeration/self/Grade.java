f2b282530c159380d15d7ce7cb60ea9ba38842a9
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-15 23:16:41 +0900

    🐱  열거형 내부에 데이터를 저장하고 메서드를 정의.

--- javaInt1Lecture/enumeration/self/Grade.java
+++ javaInt1Lecture/enumeration/self/Grade.java
@@ -1,5 +1,19 @@
 package javaInt1Lecture.enumeration.self;
 
 public enum Grade {
-    BASIC, GOLD, DIAMOND
+    BASIC(10), GOLD(20), DIAMOND(30);
+
+    private final int discountPercent;
+
+    Grade(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public int calculateDiscount(int price) {
+        return price * discountPercent / 100;
+    }
 }
