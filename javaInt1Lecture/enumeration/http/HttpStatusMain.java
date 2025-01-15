2de458fae7c2cde4b537dab5102dab5663f4eef5
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-15 23:41:14 +0900

    🐴 Main 클래스에 Scanner 변수 생성.

---
+++ javaInt1Lecture/enumeration/http/HttpStatusMain.java
@@ -0,0 +1,11 @@
+package javaInt1Lecture.enumeration.http;
+
+import java.util.Scanner;
+
+public class HttpStatusMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("HTTP CODE: ");
+        
+    }
+}
