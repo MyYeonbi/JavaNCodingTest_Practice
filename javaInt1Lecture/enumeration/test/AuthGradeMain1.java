09d7cabf13464e9305c3d3a5de1eefdffc9a36a2
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-15 17:04:56 +0900

    🐶 AuthGrade enum 생성 / Main 클래스에 Scanner를 생성하여 등급을 입력받음.

---
+++ javaInt1Lecture/enumeration/test/AuthGradeMain1.java
@@ -0,0 +1,12 @@
+package javaInt1Lecture.enumeration.test;
+
+public class AuthGradeMain1 {
+/*
+    public static void main(String[] args) {
+        AuthGrade[] values = AuthGrade.values();
+        for (AuthGrade a : values) {
+            System.out.println("grade= "+ values.name() + ", level = " + values.getlevel() + ", 설명 = " + values.getDescription() );
+
+        }
+    }*/
+}
---
+++ javaInt1Lecture/enumeration/test/AuthGradeMain2.java
@@ -0,0 +1,12 @@
+package javaInt1Lecture.enumeration.test;
+
+import java.util.Scanner;
+public class AuthGradeMain2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("당신의 등급을 입력하세요[GUEST, LOGIN, ADMIN]");
+        String grade = scanner.nextLine();
+        
+    }
+}
