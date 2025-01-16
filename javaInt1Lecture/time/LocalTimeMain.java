9019ed05f3568d4e84a28b1b9f472b7906d4eb9b
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-16 14:33:40 +0900

    🕐 ofTime.plusSeconds(더할 초 시간) : 지정 시간 +초

--- javaInt1Lecture/time/LocalTimeMain.java
+++ javaInt1Lecture/time/LocalTimeMain.java
@@ -10,7 +10,9 @@ public class LocalTimeMain {
         System.out.println("현재 시간 = " + nowTime);
         System.out.println("지정 시간 = " + ofTime);
 
-        
+        //계산(불변)
+        LocalTime ofTimePlus = ofTime.plusSeconds(30);
+        System.out.println("지정 시간 +30s = " + ofTimePlus);
 
     }
 }
