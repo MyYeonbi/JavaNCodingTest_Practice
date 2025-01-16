419b4bdbfcd69cabee924b72a6c1584441fdbe5e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-16 12:12:12 +0900

    🕐 < 섹션7. 날짜와 시간 >_ LocalDate.now(); 사용

---
+++ javaInt1Lecture/time/LocalDateTime.java
@@ -0,0 +1,10 @@
+package javaInt1Lecture.time;
+
+import java.time.LocalDate;
+
+public class LocalDateTime {
+    public static void main(String[] args) {
+        LocalDate nowDate = LocalDate.now();
+        System.out.println("오늘 날짜 = " + nowDate);
+    }
+}
