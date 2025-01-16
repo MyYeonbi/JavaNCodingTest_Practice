42eea1264656e81a3bfbf928743237aa6121fb08
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-16 14:43:40 +0900

    🕐 ofDt.toLocalDate() ,  ofDt.toLocalTime() :  LocalDateTime.of()에서 지정한 날짜와 시간 분리

--- javaInt1Lecture/time/LocalDateTimeMain.java
+++ javaInt1Lecture/time/LocalDateTimeMain.java
@@ -1,5 +1,7 @@
 package javaInt1Lecture.time;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class LocalDateTimeMain {
 
@@ -8,6 +10,12 @@ public class LocalDateTimeMain {
         LocalDateTime ofDt = LocalDateTime.of(2016, 8, 16, 8, 10, 1);
         System.out.println("현재 날짜시간 : " + nowDt);
         System.out.println("지정 날짜시간 : " + ofDt);
+
+        // 날짜와 시간 분리
+        LocalDate localDate = ofDt.toLocalDate();
+        LocalTime localTime = ofDt.toLocalTime();
+        System.out.println("--  localDate : " + localDate);
+        System.out.println("localTime : " + localTime);
 
 
     }
