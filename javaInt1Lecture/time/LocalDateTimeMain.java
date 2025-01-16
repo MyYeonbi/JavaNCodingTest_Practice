9e937ce62cefb29ffdf35731e9ac992664f63d72
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-16 14:40:29 +0900

    🕐 LocalDateTime.of(년,월,일,시간, 분,초)  : 지정 날짜 시간

--- javaInt1Lecture/time/LocalDateTimeMain.java
+++ javaInt1Lecture/time/LocalDateTimeMain.java
@@ -7,6 +7,7 @@ public class LocalDateTimeMain {
    LocalDateTime nowDt = LocalDateTime.now();
         LocalDateTime ofDt = LocalDateTime.of(2016, 8, 16, 8, 10, 1);
         System.out.println("현재 날짜시간 : " + nowDt);
+        System.out.println("지정 날짜시간 : " + ofDt);
 
 
     }
