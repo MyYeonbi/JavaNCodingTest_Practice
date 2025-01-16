fecdbd57ff90e76b569daec7b410db2a2170d8bf
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-16 15:58:08 +0900

    🕐 `LocalDateTime.of()`를 사용하여 2030년 1월 1일 13:30:50의 특정 날짜와 시간 생성. - `ZonedDateTime.of()`를 통해 Asia/Seoul 시간대를 적용한 ZonedDateTime 객체 생성.

--- javaInt1Lecture/time/ZonedDateTImeMain.java
+++ javaInt1Lecture/time/ZonedDateTImeMain.java
@@ -1,11 +1,18 @@
 package javaInt1Lecture.time;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 
 public class ZonedDateTImeMain {
 
     public static void main(String[] args) {
         ZonedDateTime nowZdt = ZonedDateTime.now();
         System.out.println("nowZdt" + nowZdt);
+
+        LocalDateTime ldt = LocalDateTime.of(2030, 1, 1, 13, 30, 50);
+        ZonedDateTime zdt1 = ZonedDateTime.of(ldt, ZoneId.of("Asia/Seoul"));
+        System.out.println("zdt1" + zdt1);
+
     }
 }
