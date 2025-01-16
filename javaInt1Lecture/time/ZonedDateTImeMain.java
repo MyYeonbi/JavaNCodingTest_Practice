bec3070d3820a3728faa89e0940b5a8c7a17399b
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-16 15:52:31 +0900

    🕐 `ZonedDateTime.now()`를 사용하여 현재 시스템 시간대의 날짜와 시간을 가져오는 로직 구현.

---
+++ javaInt1Lecture/time/ZonedDateTImeMain.java
@@ -0,0 +1,11 @@
+package javaInt1Lecture.time;
+
+import java.time.ZonedDateTime;
+
+public class ZonedDateTImeMain {
+
+    public static void main(String[] args) {
+        ZonedDateTime nowZdt = ZonedDateTime.now();
+        System.out.println("nowZdt" + nowZdt);
+    }
+}
