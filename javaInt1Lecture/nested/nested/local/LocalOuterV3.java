6abfddc22a1b91a56f3e3c9db80c5dc71830e08d
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-17 17:21:14 +0900

    🌺 process() 메서드의 반환 타입을 기존 'void'를 삭제하고, Printer로 명시_ 디버깅 해결 👍

--- javaInt1Lecture/nested/nested/local/LocalOuterV3.java
+++ javaInt1Lecture/nested/nested/local/LocalOuterV3.java
@@ -4,7 +4,7 @@ public class LocalOuterV3 {
 
     private int outInstanceVar = 3;
 
-    public void process(int paramVar) {
+    public Printer process(int paramVar) {
         int localVar = 1; // 지역 변수는  스택 프레임이 종료되는 순간 함께 제거된다.
 
         class LocalPrinter implements Printer {
