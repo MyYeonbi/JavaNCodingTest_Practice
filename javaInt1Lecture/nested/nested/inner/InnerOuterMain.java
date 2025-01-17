3cad05dbda95c9ebe2df419d97c44b372a03796f
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-17 12:48:55 +0900

    🌴 내부 클래스 인스턴스 생성 및 클래스 정보 출력 기능 추가

--- javaInt1Lecture/nested/nested/inner/InnerOuterMain.java
+++ javaInt1Lecture/nested/nested/inner/InnerOuterMain.java
@@ -5,5 +5,8 @@ public class InnerOuterMain {
     public static void main(String[] args) {
         InnerOuter outer = new InnerOuter();
         InnerOuter.Inner inner = outer.new Inner();
+        inner.print();
+
+        System.out.println("innerClass = " + inner.getClass());
     }
 }
