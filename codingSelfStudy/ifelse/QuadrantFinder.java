da9d877ead7f49219fd8bba0b0c00c91ae7aa3b1
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-17 19:27:31 +0900

    🌗 ⭕ 컴파일 오류 해결 👍 _ if-else 문에서 모든 경우의 수를 담아야한다. 그래서 마지막은 else if가 아닌 조건문  없는 else를 넣어 모든 경우를 커버하자.

--- codingSelfStudy/ifelse/QuadrantFinder.java
+++ codingSelfStudy/ifelse/QuadrantFinder.java
@@ -8,7 +8,7 @@ public class QuadrantFinder {
             return 2;
         } else if (dot[0] <0 && dot[1] <0) {
             return 3;
-        } else (dot[0]>0 && dot[1] <0){
+        } else {
             return 4;
         }
     }
