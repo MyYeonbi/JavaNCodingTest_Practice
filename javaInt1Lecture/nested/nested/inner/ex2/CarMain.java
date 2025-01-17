e21c5159babca8034ac57760f5346134405c2de4
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-17 15:14:17 +0900

    ☀️ ⭕ 매개변수 전달할때 +가 아닌 ,로 해줘야 한다. _디버깅 해결 👍

--- javaInt1Lecture/nested/nested/inner/ex2/CarMain.java
+++ javaInt1Lecture/nested/nested/inner/ex2/CarMain.java
@@ -3,7 +3,7 @@ package javaInt1Lecture.nested.nested.inner.ex2;
 public class CarMain {
 
     public static void main(String[] args) {
-        Car myCar = new Car("Model Y" + 100);
+        Car myCar = new Car("Model Y" , 100);
         myCar.start();
     }
 }
