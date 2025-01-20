a1e10586c4a7d28eb3d4c91dab166eb5434ea6e2
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-20 23:00:58 +0900

    📫 Object로 다양한 타입을 받을시, 잘못된 타입의 인수 전달시 오류가 나지 않아 문제가 생김.

--- javaInt2Lecture/generic/ex1/BoxMain2.java
+++ javaInt2Lecture/generic/ex1/BoxMain2.java
@@ -12,6 +12,10 @@ public class BoxMain2 {
         ObjectBox stringBox = new ObjectBox();
         stringBox.set("Hello");
         String string = (String) stringBox.get(); // Object -> String 캐스팅
+        System.out.println("string = " + string);
+
+        // 잘못된 타입의 인수 전달시
+        integerBox.set("문자가 왜 integer박스에 들어가죠?");
 
 
 
