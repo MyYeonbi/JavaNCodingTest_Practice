73af10df6b6ac91df00d19968c456d791deb519d
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-20 22:54:42 +0900

    📫 StringBox도 Object 객체를 String으로 다운캐스팅 하여 변수 생성.

--- javaInt2Lecture/generic/ex1/BoxMain2.java
+++ javaInt2Lecture/generic/ex1/BoxMain2.java
@@ -2,12 +2,17 @@ package javaInt2Lecture.generic.ex1;
 
 public class BoxMain2 {
     public static void main(String[] args) {
-        ObjectBox objectBox = new ObjectBox();
-        objectBox.set(10);
-        Object object = objectBox.get();
-        Integer integer = (Integer) object;
-        Integer integer2 = (Integer) objectBox.get();
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get();
+
+        Integer integer2 = (Integer) integerBox.get(); // Object -> Integer 캐스팅
         System.out.println("integer = " + integer);
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("Hello");
+        String string = (String) stringBox.get(); // Object -> String 캐스팅
+
 
 
     }
