7a0c72c1b224f65fcd06649c5be4f1e44b3741b7
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-20 21:41:08 +0900

    📥 integerBox의 get 메서드를 이용하여 value값 출력.

--- javaInt2Lecture/generic/ex1/BoxMain1.java
+++ javaInt2Lecture/generic/ex1/BoxMain1.java
@@ -5,5 +5,7 @@ public class BoxMain1 {
     public static void main(String[] args) {
         IntegerBox integerBox = new IntegerBox();
         integerBox.set(10); // 오토박싱
+        Integer integer = integerBox.get();
+        System.out.println("integer = " + integer);
     }
 }
