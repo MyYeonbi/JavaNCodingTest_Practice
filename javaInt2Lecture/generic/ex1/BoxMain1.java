13b62bc606a32bd201a5c04c99fd087f756e03cc
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-20 21:39:33 +0900

    📥 IntegerBox 구현에서 오토박스 예제 추가.
    - IntegerBox 클래스를 추가하여 Java에서의 오토박싱 동작을 시연
    - `integerBox.set(10)` 코드에서 `int`가 `Integer`로 자동 변환되는 오토박싱 적용 설명
    - 오토박싱 개념과 기본 타입을 래퍼 클래스 객체로 변환하는 역할을 강조

---
+++ javaInt2Lecture/generic/ex1/BoxMain1.java
@@ -0,0 +1,9 @@
+package javaInt2Lecture.generic.ex1;
+
+public class BoxMain1 {
+
+    public static void main(String[] args) {
+        IntegerBox integerBox = new IntegerBox();
+        integerBox.set(10); // 오토박싱
+    }
+}
