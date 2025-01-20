4e5315ddb3b9aa589781024f37288e6796ca9276
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-20 22:03:52 +0900

    📫 Object를 활용하여 다양한 타입의 클래스를 한꺼번에 처리_ Object 개념을 이슈란에 정리(복습) ✨

---
+++ javaInt2Lecture/generic/ex1/BoxMain2.java
@@ -0,0 +1,10 @@
+package javaInt2Lecture.generic.ex1;
+
+public class BoxMain2 {
+    public static void main(String[] args) {
+        ObjectBox objectBox = new ObjectBox();
+        objectBox.set(10);
+        Object object = objectBox.get();
+
+    }
+}
