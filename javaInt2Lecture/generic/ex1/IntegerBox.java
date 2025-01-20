e56823318f850aca20c7e92128f2f16c505733b1
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-20 21:32:27 +0900

    📥 int형 value값을 set메서드와 get메서드를 통해 보관하고 꺼낸다. (박스 처럼)

--- javaInt2Lecture/generic/ex1/IntegerBox.java
+++ javaInt2Lecture/generic/ex1/IntegerBox.java
@@ -4,5 +4,13 @@ public class IntegerBox {
 
     private Integer value;
 
-    
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get() {
+        return value;
+    }
+
+
 }
