bedac435ac9f446e357f9bc14d02bcc90978dd17
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-21 14:16:02 +0900

    🍋 <>를 지정하지 않는 것을 로 타입(row type)또는 원시 타입인데 , 이걸 사용하지 말아야 한다. Object 타입을 사용해야 한다면 다음과 같이 타입 인자로 Object를 지정해서 사용하면 된다.

--- javaInt2Lecture/generic/ex1/RowTypeMain.java
+++ javaInt2Lecture/generic/ex1/RowTypeMain.java
@@ -5,5 +5,9 @@ public class RowTypeMain {
     public static void main(String[] args) {
         GenericBox integerBox = new GenericBox<>();
       //  GenericBox<Object> integerBox = new GenericBox<>(); //권장.
+        integerBox.set(10);
+        Integer result = (Integer) integerBox.get();
+        System.out.println("result = " + result);
+
     }
 }
