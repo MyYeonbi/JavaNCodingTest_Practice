e8e38ba2078d40e222b9c6f32046908066893618
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-21 11:23:31 +0900

    🍋 integerBox.set("문자"); _Integer 타입만 허용, 컴파일 오류

--- javaInt2Lecture/generic/ex1/BoxMain3.java
+++ javaInt2Lecture/generic/ex1/BoxMain3.java
@@ -3,7 +3,9 @@ package javaInt2Lecture.generic.ex1;
 public class BoxMain3 {
 
     public static void main(String[] args) {
-        GenericBox<Integer> integerBox = new GenericBox<Integer>(); 
+        GenericBox<Integer> integerBox = new GenericBox<Integer>();
         //생성 시점에 T의 타입 결정.
+        integerBox.set(10);
+        integerBox.set("문자"); // Integer 타입만 허용, 컴파일 오류
     }
 }
