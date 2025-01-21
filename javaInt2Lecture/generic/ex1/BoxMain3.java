08f59f509f2dd2cfe45e1def317ef7b4534a7eef
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-21 11:24:57 +0900

    🍋  Integer integer = integerBox.get(); _ nteger 타입 반환(캐스팅 할 필요 없다.)

--- javaInt2Lecture/generic/ex1/BoxMain3.java
+++ javaInt2Lecture/generic/ex1/BoxMain3.java
@@ -6,6 +6,7 @@ public class BoxMain3 {
         GenericBox<Integer> integerBox = new GenericBox<Integer>();
         //생성 시점에 T의 타입 결정.
         integerBox.set(10);
-        integerBox.set("문자"); // Integer 타입만 허용, 컴파일 오류
+        //integerBox.set("문자"); // Integer 타입만 허용, 컴파일 오류
+        Integer integer = integerBox.get(); // Integer 타입 반환(캐스팅 할 필요 없다.)
     }
 }
