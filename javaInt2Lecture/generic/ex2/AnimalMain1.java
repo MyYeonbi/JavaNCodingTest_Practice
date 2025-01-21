28063cf9e4d840215d2a689372085f4096501253
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-21 14:49:07 +0900

    📥 Box<Dog> dogBox = new Box<>(); _ 강아지만 들어갈 수 있는 DogBox를 제네릭으로 생성 및 출력

---
+++ javaInt2Lecture/generic/ex2/AnimalMain1.java
@@ -0,0 +1,19 @@
+package javaInt2Lecture.generic.ex2;
+
+import javaInt2Lecture.generic.animal.Animal;
+import javaInt2Lecture.generic.animal.Cat;
+import javaInt2Lecture.generic.animal.Dog;
+
+public class AnimalMain1 {
+    public static void main(String[] args) {
+        Animal animal = new Animal("동물", 0);
+        Dog dog = new Dog("멍멍이", 100);
+        Cat cat = new Cat("야옹이", 50);
+
+        Box<Dog> dogBox = new Box<>();
+        dogBox.set(dog);
+        Dog findDog = dogBox.get();
+        System.out.println("findDog: " + findDog);
+
+    }
+}
