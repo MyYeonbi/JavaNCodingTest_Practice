35cc8e4145b323ef4b4cd3699fed3991edf46bcc
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-21 15:10:50 +0900

    🎠 Container<T> 제네릭 클래스에 isEmpty 메서드, setItem,getItem메서드 생성.

--- javaInt2Lecture/generic/test/ex1/Container.java
+++ javaInt2Lecture/generic/test/ex1/Container.java
@@ -1,4 +1,16 @@
 package javaInt2Lecture.generic.test.ex1;
 
 public class Container<T> {
+
+    public T isEmpty(){
+        return null;
+    }
+
+    public void setItem(T item){
+
+    }
+
+    public T getItem(){
+        return null;
+    }
 }
