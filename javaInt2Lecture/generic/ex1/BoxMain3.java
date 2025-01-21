18f0ae0d0eb20df9815a7b5256b0c123540a2145
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-21 11:21:50 +0900

    🍋  GenericBox<Integer> integerBox = new GenericBox<Integer>(); _ T자리에 타입을 넣어주면 생성 시점에 T의 타입이 결정된다.

---
+++ javaInt2Lecture/generic/ex1/BoxMain3.java
@@ -0,0 +1,9 @@
+package javaInt2Lecture.generic.ex1;
+
+public class BoxMain3 {
+
+    public static void main(String[] args) {
+        GenericBox<Integer> integerBox = new GenericBox<Integer>(); 
+        //생성 시점에 T의 타입 결정.
+    }
+}
