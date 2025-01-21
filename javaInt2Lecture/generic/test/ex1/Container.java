2d36ee761cf24015026cf8b4b74a109fd1bce816
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-21 15:23:40 +0900

    🎠 제네릭 Container 클래스 구현
    - 제네릭 타입 T를 사용하여 다양한 타입의 아이템을 저장할 수 있는 Container 클래스 구현
    - setItem(): 아이템을 설정하는 메서드 추가
    - getItem(): 아이템을 반환하는 메서드 추가
    - isEmpty(): 컨테이너가 비어있는지 확인하는 메서드 추가

--- javaInt2Lecture/generic/test/ex1/Container.java
+++ javaInt2Lecture/generic/test/ex1/Container.java
@@ -2,15 +2,19 @@ package javaInt2Lecture.generic.test.ex1;
 
 public class Container<T> {
 
-    public T isEmpty(){
-        return null;
-    }
+    private T item;
 
     public void setItem(T item){
-
+        this.item = item;
     }
 
     public T getItem(){
-        return null;
+        return item;
     }
+
+    public boolean isEmpty(){
+        return item == null;
+    }
+
+
 }
--- javaInt2Lecture/generic/test/ex1/ContainerTest.java
+++ javaInt2Lecture/generic/test/ex1/ContainerTest.java
@@ -1,15 +1,19 @@
 package javaInt2Lecture.generic.test.ex1;
 
 public class ContainerTest {
-    Container<String> stringContainer = new Container<>();
-    System.out.println("빈값 확인1: " + stringContainer.isEmpty());
+    public static void main(String[] args) {
 
-    stringContainer.setItem("data1");
-System.out.println("저장 데이터: " + stringContainer.getItem());
-System.out.println("빈값 확인2: " + stringContainer.isEmpty());
 
-    Container<Integer> integerContainer = new Container<>();
-    integerContainer.setItem(10);
-System.out.println("저장 데이터: " + integerContainer.getItem());
+        Container<String> stringContainer = new Container<>();
+        System.out.println("빈값 확인1: " + stringContainer.isEmpty());
 
+        stringContainer.setItem("data1");
+        System.out.println("저장 데이터: " + stringContainer.getItem());
+        System.out.println("빈값 확인2: " + stringContainer.isEmpty());
+
+        Container<Integer> integerContainer = new Container<>();
+        integerContainer.setItem(10);
+        System.out.println("저장 데이터: " + integerContainer.getItem());
+
+    }
 }
