bf83b0db4ea7b4100d66a37106981dd3e9843964
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 23:07:34 +0900

    🏥 그래서 메서드는 항상 Object의 기능인 메서드들만 호출 가능하다.

--- javaInt2Lecture/generic/test/ex3/AnimalHospitalV2.java
+++ javaInt2Lecture/generic/test/ex3/AnimalHospitalV2.java
@@ -13,6 +13,7 @@ public class AnimalHospitalV2 <T> {
 
     public void checkup() {
         // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. Object의 기능만 사용.
+        //그래서 메서드는 항상 Object의 기능인 메서드들만 호출 가능하다. 
         System.out.println("동물 이름 : " + animal.getName());
         System.out.println("동물 크기 : " + animal.getSize());
         animal.sound();
