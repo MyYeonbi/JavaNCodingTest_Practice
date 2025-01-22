45b16f9502365db974dcaee57bc738c9b12d2e28
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 23:05:52 +0900

    ❗ animal.getName()과 같은 메서드 호출 불가_ private T animal로 설정했으므로, T의 타입을 메서드를 정의하는 시점에는 알 수 없다.

--- javaInt2Lecture/generic/test/ex3/AnimalHospitalV2.java
+++ javaInt2Lecture/generic/test/ex3/AnimalHospitalV2.java
@@ -12,7 +12,7 @@ public class AnimalHospitalV2 <T> {
     }
 
     public void checkup() {
-        // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. 
+        // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. Object의 기능만 사용.
         System.out.println("동물 이름 : " + animal.getName());
         System.out.println("동물 크기 : " + animal.getSize());
         animal.sound();
