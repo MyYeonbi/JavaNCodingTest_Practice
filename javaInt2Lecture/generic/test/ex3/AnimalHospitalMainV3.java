5a36fcab03d03dfe69a7b718016077e4096ddfba
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 23:54:19 +0900

    🏥  제한된 타입 매개변수를 호출하는 Main 클래스

---
+++ javaInt2Lecture/generic/test/ex3/AnimalHospitalMainV3.java
@@ -0,0 +1,25 @@
+package javaInt2Lecture.generic.test.ex3;
+
+import javaInt2Lecture.generic.animal.Cat;
+import javaInt2Lecture.generic.animal.Dog;
+
+public class AnimalHospitalMainV3 {
+
+    public static void main(String[] args) {
+
+        AnimalHospitalV3 <Dog> dogHospital = new AnimalHospitalV3<>();
+        AnimalHospitalV3 <Cat> catHospital = new AnimalHospitalV3<>();
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("냐옹이1", 300);
+
+        // 개 병원
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        //고양이 병원
+        catHospital.set(cat);
+        catHospital.checkup();
+
+    }
+}
