850e1e0f4f09acb7d008dd4097a453ed34c9824f
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 23:10:22 +0900

    🏥 animal.toString();와 같은 매서드만 사용 가능하다는 의미 _ 기존 메서드를 주석처리 함.

--- javaInt2Lecture/generic/test/ex3/AnimalHospitalV2.java
+++ javaInt2Lecture/generic/test/ex3/AnimalHospitalV2.java
@@ -13,14 +13,18 @@ public class AnimalHospitalV2 <T> {
 
     public void checkup() {
         // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. Object의 기능만 사용.
-        //그래서 메서드는 항상 Object의 기능인 메서드들만 호출 가능하다. 
-        System.out.println("동물 이름 : " + animal.getName());
+        //그래서 메서드는 항상 Object의 기능인 메서드들만 호출 가능하다.
+        /*System.out.println("동물 이름 : " + animal.getName());
         System.out.println("동물 크기 : " + animal.getSize());
-        animal.sound();
+        animal.sound();*/
+
+        animal.toString();
+        animal.equals(null);
+
     }
 
-    public T bigger(Animal target) {
-
-        return animal.getSize() > target.getSize() ? animal : target;
+    public T bigger(T target) {
+return null;
+        /*return animal.getSize() > target.getSize() ? animal : target*/;
     }
 }
