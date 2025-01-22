d65d8de4e9fce86dac57263c94759696d672a36e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 20:41:00 +0900

    🔢 char 타입의 숫자를 int형으로 바꾸는 법 : -'0'

---
+++ codingSelfStudy/String/charr/CharToNumberExample.java
@@ -0,0 +1,9 @@
+package codingSelfStudy.String.charr;
+
+public class CharToNumberExample {
+    public static void main(String[] args) {
+        char c = '5';
+        int numbericValue = c - '0';
+        System.out.println(numbericValue);
+    }
+}
