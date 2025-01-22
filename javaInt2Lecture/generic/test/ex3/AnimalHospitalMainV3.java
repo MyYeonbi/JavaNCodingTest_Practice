14199dfefca1f7d8f7c38cc9201c4e3a3b637e24
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 23:58:11 +0900

    🐶  문제 2인 개 타입 변환도 다운캐스팅 없이 가능!

--- javaInt2Lecture/generic/test/ex3/AnimalHospitalMainV3.java
+++ javaInt2Lecture/generic/test/ex3/AnimalHospitalMainV3.java
@@ -22,7 +22,12 @@ public class AnimalHospitalMainV3 {
         catHospital.checkup();
 
         // 문제 1 : 개 병원에 고양이 전달
-        dogHospital.set(cat); // 다른 타입 입력 : 컴파일 오류
+        //dogHospital.set(cat); // 다른 타입 입력 : 컴파일 오류
+
+        // 문제 2 : 개 타입 변환
+        dogHospital.set(dog);
+        Dog biggerDog = dogHospital.bigger(new Dog("멍멍이2", 200));
+        System.out.println("biggerDog : " + biggerDog);
 
     }
 }
