8f9b14f4ae65dafd61ba6f1e4875d82ad000b896
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 22:21:57 +0900

    🏥 부모 클래스인 Animal로 병원 클래스 만들기.
    - Animal 타입의 동물을 관리하는 AnimalHospitalV1 클래스 추가
    - Animal 객체를 설정할 수 있는 set 메서드 구현
    - 동물의 이름, 크기를 출력하고 소리를 내는 checkup 메서드 추가
    - 두 Animal 객체 중 더 큰 크기를 가진 동물을 반환하는 bigger 메서드 구현

---
+++ javaInt2Lecture/generic/test/ex3/AnimalHospitalV1.java
@@ -0,0 +1,25 @@
+package javaInt2Lecture.generic.test.ex3;
+
+import javaInt2Lecture.generic.animal.Animal;
+import javaInt2Lecture.generic.animal.Cat;
+
+
+public class AnimalHospitalV1 {
+
+    private Animal animal;
+
+    public void set(Animal animal) {
+        this.animal = animal;
+    }
+
+    public void checkup() {
+        System.out.println("동물 이름 : " + animal.getName());
+        System.out.println("동물 크기 : " + animal.getSize());
+        animal.sound();
+    }
+
+    public Animal bigger(Animal target) {
+
+        return animal.getSize() > target.getSize() ? animal : target;
+    }
+}
