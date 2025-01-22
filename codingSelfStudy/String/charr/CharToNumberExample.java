8e699836fb36afd4519af4b7e4ff6e3d3d37021b
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-22 20:46:33 +0900

    🔢 char타입의 숫자를 int형으로 바꾸는 메서드 : Character.getNumbericValue(d);

--- codingSelfStudy/String/charr/CharToNumberExample.java
+++ codingSelfStudy/String/charr/CharToNumberExample.java
@@ -5,5 +5,10 @@ public class CharToNumberExample {
         char c = '5';
         int numbericValue = c - '0';
         System.out.println(numbericValue);
+
+        //메서드를 사용하여 숫자로 변환하는 법
+        char d = '6';
+        int numbericValue2 = Character.getNumericValue(d);
+        System.out.println(numbericValue2);
     }
 }
