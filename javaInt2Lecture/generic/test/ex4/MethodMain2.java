f7ac140177cfd022e5bb075edb32733b134ce786
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-23 22:07:17 +0900

    ⭕  public static void main(String[] args) 메서드를 감싸지 않아서 import를 잘하지 못함. _ 디버깅 해결 👍

--- javaInt2Lecture/generic/test/ex4/MethodMain2.java
+++ javaInt2Lecture/generic/test/ex4/MethodMain2.java
@@ -3,12 +3,15 @@ package javaInt2Lecture.generic.test.ex4;
 
 import javaInt2Lecture.generic.animal.Cat;
 import javaInt2Lecture.generic.animal.Dog;
+import javaInt2Lecture.generic.test.ex4.AnimalMethod;
+
 
 public class MethodMain2 {
-    Dog dog = new Dog("멍멍이", 100);
-    Cat cat = new Cat("냐옹이", 100);
+    public static void main(String[] args) {
+        Dog dog = new Dog("멍멍이", 100);
+        Cat cat = new Cat("냐옹이", 100);
+        AnimalMethod.checkup(dog);
+        AnimalMethod.checkup(cat);
 
-    AnimalMethod.checkup(dog);
-    AnimalMethod.checkup(cat);
-
+    }
 }
