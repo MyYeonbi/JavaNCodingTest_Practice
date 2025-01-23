7b98cedde1ec32e918e7b2c62e76ddcc4a63af1a
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-23 17:24:36 +0900

    😽 T타입을 Number로 제한을 걸어두었던 메서드도 동일하게 호출 가능.

--- javaInt2Lecture/generic/test/ex4/MethodMain1.java
+++ javaInt2Lecture/generic/test/ex4/MethodMain1.java
@@ -4,11 +4,12 @@ public class MethodMain1 {
     public static void main(String[] args) {
         Integer i = 10;
         Object object = GenericMethod.objMethod(i);
-        Integer result = (Integer) GenericMethod.objMethod(i);
+       /* Integer result = (Integer) GenericMethod.objMethod(i);*/
 
         // 타입 인자(Type Arqument) 명시적 전달
         System.out.println("명시적 타입 인자 명시적 전달");
-        GenericMethod.<Integer>genericMethod(i);
+        Integer result = GenericMethod.<Integer>genericMethod(i);
+        Integer integerValue = GenericMethod.<Integer>numberMethod(10);
 
 
     }
