b2fae4331456f5f48a6ede6f51da0489ec12f5b3
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-23 17:38:37 +0900

    😽 제네릭 메서드 호출할때 매개변수에 입력된 타입을 통해 <>안에 표기 없이도 인식이 가능하다.

--- javaInt2Lecture/generic/test/ex4/MethodMain1.java
+++ javaInt2Lecture/generic/test/ex4/MethodMain1.java
@@ -12,6 +12,13 @@ public class MethodMain1 {
         Integer integerValue = GenericMethod.<Integer>numberMethod(10);
         Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
 
+        Integer result1 = GenericMethod.genericMethod(i);
+        Integer integerValue1 = GenericMethod.<Integer>numberMethod(10);
+        Double doubleValue1 = GenericMethod.<Double>numberMethod(20.0);
+
+
+
+
 
     }
 }
