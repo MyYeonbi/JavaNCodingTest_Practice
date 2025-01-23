9e41c1ba78bf27e41fe84e09b406f6e8c1e3c58d
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-23 17:22:05 +0900

    😽 제네릭 메서드를 호출할 때, 타입을 정하기 가능!_ GenericMethod.<Integer>genericMethod(i);

--- javaInt2Lecture/generic/test/ex4/MethodMain1.java
+++ javaInt2Lecture/generic/test/ex4/MethodMain1.java
@@ -6,5 +6,10 @@ public class MethodMain1 {
         Object object = GenericMethod.objMethod(i);
         Integer result = (Integer) GenericMethod.objMethod(i);
 
+        // 타입 인자(Type Arqument) 명시적 전달
+        System.out.println("명시적 타입 인자 명시적 전달");
+        GenericMethod.<Integer>genericMethod(i);
+
+
     }
 }
