3853ba108f1c053aa88113103a12e7ff363b5318
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-23 11:58:14 +0900

    😽 <자바 중급2_ 섹션3 제네릭2_ 제네릭 메서드> Object를 반환하는 메서드 생성.

---
+++ javaInt2Lecture/generic/test/ex4/GenericMethod.java
@@ -0,0 +1,9 @@
+package javaInt2Lecture.generic.test.ex4;
+
+public class GenericMethod {
+
+    public static Object objMethod(Object obj) {
+        System.out.println("Object print = "+ obj);
+        return obj;
+    }
+}
