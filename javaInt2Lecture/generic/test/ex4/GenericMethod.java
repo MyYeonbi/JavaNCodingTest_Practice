c215c132c6d4df69257881b76213a2cf529e051d
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-23 17:29:16 +0900

    😽 제네릭 타입은 클래스에서 지정하는 것. 제네릭 메서드는 메서드에 내에서만 적용되는 것.

--- javaInt2Lecture/generic/test/ex4/GenericMethod.java
+++ javaInt2Lecture/generic/test/ex4/GenericMethod.java
@@ -8,12 +8,12 @@ public class GenericMethod {
     }
 
     public static <T> T genericMethod(T t) {
-        System.out.println("Object print = "+ t);
+        System.out.println("generic print = "+ t);
         return t;
     }
 
     public static <T extends  Number> T numberMethod(T t) {
-        System.out.println("Object print = "+ t);
+        System.out.println("Number print = "+ t);
         return t;
     }
 }
