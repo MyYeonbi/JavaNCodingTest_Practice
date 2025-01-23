02cc70339c83f22dbeebd2d1a59c20eccd96f009
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-23 17:18:19 +0900

    😽 매개변수가 Object인 genericMethod에서 변수를 지정하려면 다운캐스팅 필요.

---
+++ javaInt2Lecture/generic/test/ex4/MethodMain1.java
@@ -0,0 +1,10 @@
+package javaInt2Lecture.generic.test.ex4;
+
+public class MethodMain1 {
+    public static void main(String[] args) {
+        Integer i = 10;
+        Object object = GenericMethod.objMethod(i);
+        Integer result = (Integer) GenericMethod.objMethod(i);
+
+    }
+}
