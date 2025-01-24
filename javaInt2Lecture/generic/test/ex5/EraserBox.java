4bbc989ad1f2cfeca3308d5670886756ee34a2f0
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-24 15:03:23 +0900

    ⚪ 제네릭 타입 소거(Type Erasure)와 제네릭 타입의 제한 때문에 생긴 오류

--- javaInt2Lecture/generic/test/ex5/EraserBox.java
+++ javaInt2Lecture/generic/test/ex5/EraserBox.java
@@ -5,4 +5,8 @@ public class EraserBox <T>{
     public boolean instanceCheck(Object param) {
         return param instanceof T;
     }
+
+    public void create() {
+        return new T();
+    }
 }
