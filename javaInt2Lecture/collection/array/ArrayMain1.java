e1b8dfc9bb9f99cf7928dc17e2d483b49d1f6dce
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-24 22:56:52 +0900

    🎻 arr[0] = 1;는 바로 찾을수 있어 빠른 배열.

--- javaInt2Lecture/collection/array/ArrayMain1.java
+++ javaInt2Lecture/collection/array/ArrayMain1.java
@@ -4,5 +4,11 @@ public class ArrayMain1 {
 
     public static void main(String[] args) {
         int[] arr = new int[5];
+        //index 입력 : O(1)
+        System.out.println("== index 입력 : 0(1) ==");
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
     }
 }
