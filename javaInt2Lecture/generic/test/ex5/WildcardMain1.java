93504e2816f99de041319cdb1e3a8ed43530c53c
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-24 12:26:10 +0900

    🚙 타입 추론이 가능해서 Box<Dog> dogBox = new Box<>(); 제네릭 객체 생성시 오른쪽 <>는 공백으로 남겨둬도 된다.

--- javaInt2Lecture/generic/test/ex5/WildcardMain1.java
+++ javaInt2Lecture/generic/test/ex5/WildcardMain1.java
@@ -7,8 +7,8 @@ public class WildcardMain1 {
 
     public static void main(String[] args) {
         Box<Object> objBox = new Box<>();
-        Box<Dog> dogBox = new Box<Dog>();
-        Box<Cat> catBox = new Box<Cat>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
 
         dogBox.set(new Dog("멍멍이", 100));
 
