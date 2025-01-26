dc3dbfa5e787ea0fb4cb302b423c83ac42bc0a35
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-26 23:47:13 +0900

    ㊗️  LinkedList를 매개변수로 for문과 if문을 사용하여 찾으려는 전공과 일치하는지를 확인.

--- codingStudy/list/linkedList/studentMajorFilter/Main.java
+++ codingStudy/list/linkedList/studentMajorFilter/Main.java
@@ -5,7 +5,10 @@ import java.util.LinkedList;
 public class Main {
 
     public static void solution(LinkedList<Student> students) {
-        // 리스트 순회하며 전공이 전기전자인 학생 필터링 및 출력
+       /* 학생명과 전공이 추가된 LinkedList를 매개변수로 하여
+       for문을 통해 이 리스트를 순회한다. 그리고 getMajor인 get메서드를 불러와서
+       "전기전자"라는 문구와 일치하는지를 조건으로 하여 출력하게 만든다. 
+        */
         for (Student student : students) {
             if (student.getMajor().equals("전기전자")) {
                 System.out.println(student);
