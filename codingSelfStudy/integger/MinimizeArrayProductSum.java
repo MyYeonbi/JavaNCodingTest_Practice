afd854e05c36b7e183c3ff4091ed9112cfe9444e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-27 22:53:17 +0900

    🍊 2) Arrays.stream(B) : int[]를 스트림으로 변환해야 한다.

--- codingSelfStudy/integger/MinimizeArrayProductSum.java
+++ codingSelfStudy/integger/MinimizeArrayProductSum.java
@@ -12,7 +12,8 @@ public class MinimizeArrayProductSum {
         Integer[] BInteger = Arrays.stream(B).boxed().toArray(Integer[] :: new);
         /*
         1) 왜 내림차순으로 정렬을 위해 기본 자료형을 객체 타입 배열인 Integer[]로 변환해야 하냐면
-        Collections.reverseOrder()를 사용하기 위함이다. 
+        Collections.reverseOrder()를 사용하기 위함이다.
+        2) Arrays.stream(B) : int[]를 스트림으로 변환해야 한다. 스트림은 배열이나 컬렉션 등의 데이터를 처리하기 위한 연속적인 작업 흐름을 제공하는 자바의 API이다.
 
         3) .toArray(Integer[] :: new )는 자바에서 stream을 사용하여 배열로 변환할 때 사용하는 방식이다.
         * */
