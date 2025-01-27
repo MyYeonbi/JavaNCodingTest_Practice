b607e152f2f81c01ca564e10e0345ef9a9d2b838
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-27 22:54:19 +0900

    🍊 .boxed() : 기본 타입인 int를 객체 타입인 Integer로 박싱하는 것이다.

--- codingSelfStudy/integger/MinimizeArrayProductSum.java
+++ codingSelfStudy/integger/MinimizeArrayProductSum.java
@@ -14,7 +14,7 @@ public class MinimizeArrayProductSum {
         1) 왜 내림차순으로 정렬을 위해 기본 자료형을 객체 타입 배열인 Integer[]로 변환해야 하냐면
         Collections.reverseOrder()를 사용하기 위함이다.
         2) Arrays.stream(B) : int[]를 스트림으로 변환해야 한다. 스트림은 배열이나 컬렉션 등의 데이터를 처리하기 위한 연속적인 작업 흐름을 제공하는 자바의 API이다.
-
+        3) .boxed() : 기본 타입인 int를 객체 타입인 Integer로 박싱하는 것이다. 
         3) .toArray(Integer[] :: new )는 자바에서 stream을 사용하여 배열로 변환할 때 사용하는 방식이다.
         * */
         Arrays.sort(BInteger, Collections.reverseOrder());
