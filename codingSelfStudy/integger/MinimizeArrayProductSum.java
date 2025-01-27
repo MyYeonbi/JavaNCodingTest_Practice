7a1280314705c7b36503ebd6d2fc47b7cd1ceac6
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-27 22:51:38 +0900

    🍊  1) 왜 내림차순으로 정렬을 위해 기본 자료형을 객체 타입 배열인 Integer[]로 변환해야 하냐면         Collections.reverseOrder()를 사용하기 위함이다.

--- codingSelfStudy/integger/MinimizeArrayProductSum.java
+++ codingSelfStudy/integger/MinimizeArrayProductSum.java
@@ -10,6 +10,12 @@ public class MinimizeArrayProductSum {
 
         // 배열 B를 내림차순으로 정렬
         Integer[] BInteger = Arrays.stream(B).boxed().toArray(Integer[] :: new);
+        /*
+        1) 왜 내림차순으로 정렬을 위해 기본 자료형을 객체 타입 배열인 Integer[]로 변환해야 하냐면
+        Collections.reverseOrder()를 사용하기 위함이다. 
+
+        3) .toArray(Integer[] :: new )는 자바에서 stream을 사용하여 배열로 변환할 때 사용하는 방식이다.
+        * */
         Arrays.sort(BInteger, Collections.reverseOrder());
 
         int answer = 0;
