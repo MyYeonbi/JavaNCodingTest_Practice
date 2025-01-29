e38d08ee2b3b2771650f34391504cf7ef6cfaad1
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-29 23:55:04 +0900

    🍚 외부 반복문(시작 숫자 설정)

--- codingSelfStudy/math/twopointer/NumberRepresentation.java
+++ codingSelfStudy/math/twopointer/NumberRepresentation.java
@@ -3,9 +3,10 @@ package codingSelfStudy.math.twopointer;
 public class NumberRepresentation {
     public int solution(int n) {
         int count = 0;
-        // 연속된 자연수의 합으로 n을 만들 수 있는 경우의 수를 저장. 
+        // 연속된 자연수의 합으로 n을 만들 수 있는 경우의 수를 저장.
 
         for (int start = 1; start <= n; start++) {
+            // 외부 반복문(시작 숫자 설정)
             int sum = 0;
 
             for (int num = start; num <= n; num++){
