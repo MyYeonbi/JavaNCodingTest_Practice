910204f6cba3554e1c69cb3286209c713c74ce88
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-29 23:31:36 +0900

    💻  몇 번 변환을 했는지 저장하는 변수.

--- codingSelfStudy/binary/ZeroRemoverAndBinaryConverter.java
+++ codingSelfStudy/binary/ZeroRemoverAndBinaryConverter.java
@@ -1,10 +1,17 @@
 package codingSelfStudy.binary;
 
 import java.util.Arrays;
+/* < 문제 설명 >
+* 1. 어떤 숫자가 들어오면, 숫자 안에 있는 0을 모두 없애야 한다.
+* 2. 남아있는 숫자의 길이를 다시 이진수로 바꾼다.
+* 3. 이 과정을 숫자가 "1"이 될 때까지 반복한다.
+* 4. 과정이 끝나면, 몇 번 변환을 했는지와 얼마나 많은 0을 지웠는지 리턴해야 한다. */
 
 public class ZeroRemoverAndBinaryConverter {
     public int[] solution(String s) {
+
         int count = 0;
+        // 몇 번 변환을 했는지 저장하는 변수. 
         int removeZero = 0;
 
         while (!s.equals("1")) {
