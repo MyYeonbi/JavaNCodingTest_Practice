7956a75924a713a2b269b6d1c8cb54d418e3b462
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-29 19:55:10 +0900

    😎 stack 변수를 선언하고 새로운 객체를 생성하면서, 괄호를 저장하는 스택 자료구조.

--- codingSelfStudy/stack/ValidParenthesesChecker.java
+++ codingSelfStudy/stack/ValidParenthesesChecker.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 public class ValidParenthesesChecker {
     public boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
+        //stack 변수를 선언하고 새로운 객체를 생성하면서, 괄호를 저장하는 스택 자료구조.
 
         for (char ch : s.toCharArray()) {
             if (ch == '(') {
