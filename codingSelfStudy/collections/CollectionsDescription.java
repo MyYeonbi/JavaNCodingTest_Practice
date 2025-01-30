5cb17091b4e61ca6647f0be6b615bc336eb5e54f
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-30 18:14:39 +0900

    📚 1. Collections.sort(List<T>list) - 리스트 정렬

--- codingSelfStudy/collections/CollectionsDescription.java
+++ codingSelfStudy/collections/CollectionsDescription.java
@@ -1,7 +1,19 @@
 package codingSelfStudy.collections;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /*Collections 클래스는 컬렉션(List,Set,Map)의 조작(정렬,섞기,복사,검색 등)을 도와주는 유틸리티 클래스.
 * - java.util.Collections 패키지에 포함됨.
 * - Collections 클래스는 인스턴스를 생성할 수 없는 static 메서드 모음*/
 public class CollectionsDescription {
+    public static void main(String[] args) {
+        //1. Collections.sort(List<T>list) - 리스트 정렬
+        // 리스트를 오름차순 정렬, 내부적으로 TimSort알고리즘 사용(시간복잡도 O(N log N)
+        List<Integer> numbers = Arrays.asList(5,2,9,1,3);
+
+        Collections.sort(numbers);// 오름차순 정렬
+        System.out.println(numbers);// [1, 2, 3, 5, 9]
+    }
 }
