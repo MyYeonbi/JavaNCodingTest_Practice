aab48715af2ba59f47ef0fef390538d4c2d39cd5
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-30 12:55:05 +0900

    🍗 Arrays.asList(배열 요소) : Array의 메서드 중 배열을 List 타입으로 변경하는 메서드. 리스트가 생성되지만 크기를 변경할 수 없음!

---
+++ codingSelfStudy/arrayy/method/AsList.java
@@ -0,0 +1,11 @@
+package codingSelfStudy.arrayy.method;
+import java.util.Arrays;
+import java.util.List;
+
+public class AsList {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        System.out.println(list);
+    }
+
+}
