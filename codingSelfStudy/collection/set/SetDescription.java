74ec86638490da1cd4df6a6dd09d47c1ae8237bb
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-30 17:49:09 +0900

    📙 대표 구현체 : HashSet, TreeSet, LinkedHashSet

--- codingSelfStudy/collection/set/SetDescription.java
+++ codingSelfStudy/collection/set/SetDescription.java
@@ -6,6 +6,12 @@ public class SetDescription {
     * 2) 요소의 저장 순서를 유지하지 않음
     * 3) 정렬이 필요한 경우 TreeSet 사용*/
     public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("A");
+        set.add("B");
+        set.add("A"); // 중복 값 무시됨.
+
+        System.out.println(set);  // [A, B] (순서는 랜덤)
 
     }
 }
