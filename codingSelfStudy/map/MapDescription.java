24ce01982f222628b2fe756f6fd241d318d7a219
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-30 17:53:03 +0900

    👫 Map의 특징_key-value와 쌍으로 저장, 요소 순서 유지x (LinkedHashMap은 순서 유지). 키는 중복 불가능, 값은 중복 가능.

---
+++ codingSelfStudy/map/MapDescription.java
@@ -0,0 +1,11 @@
+package codingSelfStudy.map;
+import java.util.*;
+
+/*Map은 Colloection 인터페이스를 직접 구현하지 않음!
+* - key- value(키-값)형태로 데이터 저장
+* - 키는 중복 불가능. 값은 중복 가능.
+* - 요소의 순서 유지 X (LinkedHashMap은 순서 유지)*/
+public class MapDescription {
+
+
+}
