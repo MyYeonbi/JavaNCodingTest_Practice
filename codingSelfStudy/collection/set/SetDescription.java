f87ad1db106745b0d314cda8d39f3bea597ab240
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-30 17:46:44 +0900

    📙  Set도 Collection을 구현하는 인터페이스._ 중복 허용X, 요소의 저장 순서를 외우지 않음, 정렬이 필요한 경우 TreeSet 사용

---
+++ codingSelfStudy/collection/set/SetDescription.java
@@ -0,0 +1,11 @@
+package codingSelfStudy.collection.set;
+import java.util.*;
+public class SetDescription {
+    /* Set도 Collection을 구현하는 인터페이스.
+    * 1) 중복 요소 허용 X
+    * 2) 요소의 저장 순서를 유지하지 않음
+    * 3) 정렬이 필요한 경우 TreeSet 사용*/
+    public static void main(String[] args) {
+
+    }
+}
