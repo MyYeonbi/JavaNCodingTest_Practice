26c6ef97e85945de285590dc99a35c54905b27e9
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-30 13:47:20 +0900

    🍶 Arrays.stream(B) - 연속된 데이터 흐름을 다룰 수 있는 기능 제공.

---
+++ codingSelfStudy/arrayy/method/Stream.java
@@ -0,0 +1,8 @@
+package codingSelfStudy.arrayy.method;
+
+public class Stream {
+    /* Arrays.stream(B)
+    * 1. 무엇을 하나 - 배열 B를 스트림으로 변환. Stream은 "연속된 데이터 흐름을 다룰 수 있는 기능"을 제공.
+    * 2. 유의점 : int[]을 Stream<int[]>가 아니라 IntStream으로 변환함. 즉, 기본형 int에 특화된 IntStream 제공.
+    * */
+}
