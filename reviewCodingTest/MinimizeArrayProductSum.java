4582c72b1f398185115b7cec7fc69ae100300bd4
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-31 10:37:55 +0900

    🍒 오름차순 정렬 : Arrays.sort(A) // 내림차순 정렬 : int배열을 Integer 배열로 바꿔야지 Collections 인터페이스의 reverseOrder메서드를 사용할 수 있다.

--- reviewCodingTest/MinimizeArrayProductSum.java
+++ reviewCodingTest/MinimizeArrayProductSum.java
@@ -4,4 +4,12 @@ import java.util.Arrays;
 import java.util.Collections;
 /* 두개의 int 배열을 각각 쌍으로 곱하여 모두 더한 값이 최소가 되게 하는 문제*/
 public class MinimizeArrayProductSum {
+    public int solution (int[]A, int[]B) {
+        //1. A를 오름차순으로 정렬
+        Arrays.sort(A);
+
+        //2. B를 내림차순으로 정렬
+        Integer[] BInteger = Arrays.stream(B).boxed().toArray(Integer[] :: new);
+        Arrays.sort(BInteger,Collections.reverseOrder());
+    }
 }
