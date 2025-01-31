ae506b9d81c7a9931a9974df9211e0db7e746597
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-31 10:29:02 +0900

    🌺 int형을 String 타입으로 바꾸는 가장 간단한 방법 : String타입의 " "을 더해준다.

--- reviewCodingTest/FindMinMax.java
+++ reviewCodingTest/FindMinMax.java
@@ -1,7 +1,7 @@
 package reviewCodingTest;
 
 public class FindMinMax {
-    public String solution (String s) {
+    public String solution(String s) {
         //1. 입력 문자열을 공백 기준으로 나누기. - String 배열로 값을 받아야 하며, 메서드 호출 방식은 변수이름.split("나누는 기준")..
         String[] numbers = s.split(" ");
 
@@ -11,14 +11,17 @@ public class FindMinMax {
 
 
         // 3. 배열 순회하면서 최댓값, 최솟값 찾기
-        for(String str : numbers) {
-            try{
+        for (String str : numbers) {
+            try {
                 int num = Integer.parseInt(str);
-                if(num  < min) min = num;
+                if (num < min) min = num;
                 if (num > max) max = num;
-            } catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 throw new
                         IllegalArgumentException("Invalid input :" + str);
             }
+        }
+        //4. 결과를 "min max" 형식의 문자열로 변환
+        return min + " " + max;
     }
 }
