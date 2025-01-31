8a9a6c5e1ba584b3c215c1171a09132475419fb3
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-31 12:28:37 +0900

    😘 1. 리턴할 String을 담을 StringBuilder.

--- reviewCodingTest/JadenCaseConverter.java
+++ reviewCodingTest/JadenCaseConverter.java
@@ -2,4 +2,7 @@ package reviewCodingTest;
 /*문자열의 각 단어에서 첫 문자는 대문자로, 나머지 문자는 소문자로 만드는 문제.*/
 
 public class JadenCaseConverter {
+    public String solution(String s) {
+        //1. 리턴할 String을 담을 StringBuilder.
+    }
 }
