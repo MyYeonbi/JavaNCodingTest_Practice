95f449cc5e76d42cc83761d3e404dc96f714bb8e
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-31 10:07:12 +0900

    🌺 (코테 복습) String열의 공백 기준으로 숫자를 최대 최소값을 String 형식으로 리턴하기.

---
+++ reviewCodingTest/FindMinMax.java
@@ -0,0 +1,9 @@
+package reviewCodingTest;
+
+public class FindMinMax {
+    public String solution (String s) {
+        //1. 입력 문자열을 공백 기준으로 나누기. - String 배열로 값을 받아야 하며, 메서드 호출 방식은 변수이름.split("나누는 기준")
+        String[] numbers = s.split(" ");
+
+    }
+}
