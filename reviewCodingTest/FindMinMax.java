85124ece5d2178a3141f92e2482c52b5b17de29a
MyYeonbi <yeonbi_kim@icloud.com>
2025-01-31 10:11:06 +0900

    🌺 최대값과 최소값을 담을 변수 선언 및 초기화. 초기화 값은 min은 Integer.MAX_VALUE, max는 Integer.MIN_VALUE로 바로 대체 가능하게끔 만들어준다.

--- reviewCodingTest/FindMinMax.java
+++ reviewCodingTest/FindMinMax.java
@@ -5,5 +5,10 @@ public class FindMinMax {
         //1. 입력 문자열을 공백 기준으로 나누기. - String 배열로 값을 받아야 하며, 메서드 호출 방식은 변수이름.split("나누는 기준")..
         String[] numbers = s.split(" ");
 
+        //2. 초기값 설정
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+
     }
 }
