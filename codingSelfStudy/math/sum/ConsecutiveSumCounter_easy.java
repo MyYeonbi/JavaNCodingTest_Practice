d980860750553b0922de9cccc98e0ee292a6cf29
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-02 20:24:43 +0900

    🍞 n을 만들기 위해 모든 숫자를 시작점으로 본다.start라는 변수를 1로 시작해서, n까지 하나씩 증가시킨다.

--- codingSelfStudy/math/sum/ConsecutiveSumCounter_easy.java
+++ codingSelfStudy/math/sum/ConsecutiveSumCounter_easy.java
@@ -3,9 +3,10 @@ package codingSelfStudy.math.sum;
 public class ConsecutiveSumCounter_easy {
     public int solution(int n) {
         int count = 0;
-        // 방법의 개수를 저장. n을 연속된 숫자로 만드는 방법을 찾을 때마다 count를 1씩 늘린다. 
+        // 방법의 개수를 저장. n을 연속된 숫자로 만드는 방법을 찾을 때마다 count를 1씩 늘린다.
 
         for (int start = 1; start <= n; start++) {
+            // n을 만들기 위해 모든 숫자를 시작점으로 본다.start라는 변수를 1로 시작해서, n까지 하나씩 증가시킨다.
 
             int sum = 0;
 
