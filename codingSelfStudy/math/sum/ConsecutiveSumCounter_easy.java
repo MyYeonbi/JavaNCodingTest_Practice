911ad39220b37d7ea99cca9223a61a0dbf7e5ac9
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-02 20:23:11 +0900

    🍞 방법의 개수를 저장. n을 연속된 숫자로 만드는 방법을 찾을 때마다 count를 1씩 늘린다.

--- codingSelfStudy/math/sum/ConsecutiveSumCounter_easy.java
+++ codingSelfStudy/math/sum/ConsecutiveSumCounter_easy.java
@@ -1,10 +1,12 @@
 package codingSelfStudy.math.sum;
-
+/*숫자  n 을 연속된 자연수들의 합으로 표현할 수 있는 방법을 모두 찾고 방법의 개수를 리턴하기.!*/
 public class ConsecutiveSumCounter_easy {
     public int solution(int n) {
         int count = 0;
+        // 방법의 개수를 저장. n을 연속된 숫자로 만드는 방법을 찾을 때마다 count를 1씩 늘린다. 
 
         for (int start = 1; start <= n; start++) {
+
             int sum = 0;
 
             for (int num = start; sum < n; num++) {
