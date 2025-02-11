cecdfa3f1fd6630593b3d462f37b0af5ae02e9ab
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-11 09:03:32 +0900

    🚗 (코테) 정수를 담고 있는 배열 arr의 평균값을 return하는 함수

---
+++ codingSelfStudy/arrayy/method/AverageCalculator.java
@@ -0,0 +1,19 @@
+package codingSelfStudy.arrayy.method;
+/*정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+ */
+public class AverageCalculator {
+
+    public double solution(int[] arr) {
+        int sum = 0;
+        for(int n : arr) {
+            sum += n;
+        }
+        return (double) sum / arr.length;
+
+    }
+
+    public static void main(String[] args) {
+        AverageCalculator a = new AverageCalculator();
+        System.out.println(a.solution(new int[1,3,4,5]);
+    }
+}
