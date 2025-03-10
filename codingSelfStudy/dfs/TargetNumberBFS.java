0cf69bb8d299df07ac4c52d66231a772122ff168
MyYeonbi <yeonbi_kim@icloud.com>
2025-03-10 16:08:57 +0900

    🔗 BFS를 활용한 타겟 넘버 문제 해결
    - BFS(너비 우선 탐색)를 이용하여 주어진 숫자 배열에서 target을 만드는 경우의 수 계산
    - Queue를 사용해 가능한 모든 연산 경우를 탐색
    - sum과 index 값을 관리하며 + 또는 - 연산을 수행
    - 최종적으로 target과 일치하는 경우 answer 값 증가

---
+++ codingSelfStudy/dfs/TargetNumberBFS.java
@@ -0,0 +1,25 @@
+package codingSelfStudy.dfs;
+
+import java.util.*;
+
+class TargetNumberBFS {
+  public int solution(int[] numbers, int target) {
+    int answer = 0;
+    Queue<int[]> queue = new LinkedList<>();
+    queue.add(new int[]{0, 0}); // (현재 합, 인덱스)
+
+    while (!queue.isEmpty()) {
+      int[] current = queue.poll();
+      int sum = current[0];
+
+      if (index == numbers.length) { // 모든 숫자를 사용한 경우
+        if (sum == target) answer++;
+      } else {
+        queue.add(new int[]{sum + numbers[index], index + 1});
+        queue.add(new int[]{sum - numbers[index], index + 1});
+      }
+    }
+
+    return answer;
+  }
+}
