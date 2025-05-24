ddb929daf6194a4707e7cb2d3a6965f160bb530e
MyYeonbi <yeonbi_kim@icloud.com>
2025-05-24 14:47:53 +0900

    (코테) 큐 문제. 마지막 남는 카드 출력

---
+++ codingStudy/queue1/Que.java
@@ -0,0 +1,25 @@
+package codingStudy.queue1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Que {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    Queue<Integer> queue = new LinkedList<>();
+
+    for(int i = 1; i <= n; i++) {
+      queue.offer(i);
+    }
+
+    while(queue.size() > 1) {
+      queue.poll(); // 제일 위 카드 버리기
+      queue.offer(queue.poll()); // 다음 카드를 아래로 옮기기
+    }
+
+    System.out.println(queue.peek());
+  }
+
+}
