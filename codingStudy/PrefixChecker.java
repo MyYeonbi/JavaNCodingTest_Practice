9ec06c5781dad265eb652e318b80e3c5bbded384
MyYeonbi <yeonbi_kim@icloud.com>
2025-06-08 14:47:13 +0900

    (코테) 접두사면 1 아니면 0을 반환하는 문제

---
+++ codingStudy/PrefixChecker.java
@@ -0,0 +1,13 @@
+package codingStudy;
+
+public class PrefixChecker {
+  public int solution(String my_string, String is_prefix) {
+    return my_string.startsWith(is_prefix) ? 1 : 0;
+  }
+
+  class Solution {
+    public int solution(String my_string, String is_prefix) {
+      return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+  }
+}
